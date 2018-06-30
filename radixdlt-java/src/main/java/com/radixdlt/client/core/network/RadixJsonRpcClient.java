c30685a35117a43462a9ad99e85010a480a80c32
Joshua Primero <josh@radixdlt.com>
2018-06-30 18:10:22 -0400

    Add Javadocs to RadixJsonRpcClient

--- radixdlt-java/src/main/java/com/radixdlt/client/core/network/RadixJsonRpcClient.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/core/network/RadixJsonRpcClient.java
@@ -1,6 +1,5 @@
 package com.radixdlt.client.core.network;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -26,21 +25,29 @@ import org.slf4j.LoggerFactory;
 public class RadixJsonRpcClient {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RadixJsonRpcClient.class);
 
-	private final Gson gson = RadixJson.getGson();
-	private final JsonParser parser = new JsonParser();
+	/**
+	 * The websocket this is wrapping
+	 */
 	private final WebSocketClient wsClient;
+
+	/**
+	 * Hot observable of messages received through the websocket
+	 */
 	private final Observable<JsonObject> messages;
 
 	public RadixJsonRpcClient(WebSocketClient wsClient) {
-
 		this.wsClient = wsClient;
+
+		final JsonParser parser = new JsonParser();
 		this.messages = this.wsClient.getMessages()
 			.map(msg -> parser.parse(msg).getAsJsonObject())
 			.publish()
 			.refCount();
 	}
 
-
+	/**
+	 * @return URL which websocket is connected to
+	 */
 	public String getLocation() {
 		return wsClient.getLocation();
 	}
@@ -49,10 +56,25 @@ public class RadixJsonRpcClient {
 		return wsClient.getStatus();
 	}
 
+	/**
+	 * Attempts to close the websocket this client is connected to.
+	 * If there are still observers connected to the websocket closing
+	 * will not occur.
+	 *
+	 * @return true if websocket was successfully closed, false otherwise
+	 */
 	public boolean tryClose() {
 		return this.wsClient.close();
 	}
 
+	/**
+	 * Helper method for calling a JSON RPC method
+	 *
+	 * @param method name of JSON RPC method
+	 * @param returnType the type this method is expected to return
+	 * @param <T>
+	 * @return response from rpc method
+	 */
 	private <T> Single<T> callJsonRpcMethod(String method, TypeToken<T> returnType) {
 		return this.wsClient.connect().andThen(
 			Single.<T>create(emitter -> {
@@ -68,7 +90,7 @@ public class RadixJsonRpcClient {
 					.filter(msg -> msg.get("id").getAsString().equals(uuid))
 					.firstOrError()
 					.doOnSubscribe(disposable -> {
-						boolean sendSuccess = wsClient.send(gson.toJson(requestObject));
+						boolean sendSuccess = wsClient.send(RadixJson.getGson().toJson(requestObject));
 						if (!sendSuccess) {
 							disposable.dispose();
 							emitter.onError(new RuntimeException("Could not connect."));
@@ -76,7 +98,9 @@ public class RadixJsonRpcClient {
 					})
 					.subscribe(msg -> {
 						if (msg.getAsJsonObject().has("result")) {
-							T data = gson.fromJson(msg.getAsJsonObject().get("result"), returnType.getType());
+							T data = RadixJson.getGson().fromJson(
+								msg.getAsJsonObject().get("result"), returnType.getType()
+							);
 							emitter.onSuccess(data);
 						} else {
 							emitter.onError(new RuntimeException(msg.getAsJsonObject().get("error").toString()));
@@ -86,14 +110,32 @@ public class RadixJsonRpcClient {
 		);
 	}
 
+	/**
+	 * Retrieve the node data for node we are connected to
+	 *
+	 * @return node data for node we are connected to
+	 */
 	public Single<NodeRunnerData> getSelf() {
 		return this.callJsonRpcMethod("Network.getSelf", new TypeToken<NodeRunnerData>() { });
 	}
 
+	/**
+	 * Retrieve list of nodes this node knows about
+	 *
+	 * @return list of nodes this node knows about
+	 */
 	public Single<List<NodeRunnerData>> getLivePeers() {
 		return this.callJsonRpcMethod("Network.getLivePeers", new TypeToken<List<NodeRunnerData>>() { });
 	}
 
+	/**
+	 *  Retrieves all atoms from a node specified by a query. This includes all past
+	 *  and future atoms. The Observable returned will never complete.
+	 *
+	 * @param atomQuery query specifying which atoms to retrieve
+	 * @param <T> atom type
+	 * @return observable of atoms
+	 */
 	public <T extends Atom> Observable<T> getAtoms(AtomQuery<T> atomQuery) {
 		return this.wsClient.connect().andThen(
 			Observable.create(emitter -> {
@@ -114,7 +156,7 @@ public class RadixJsonRpcClient {
 					.map(p -> p.get("atoms").getAsJsonArray())
 					.flatMapIterable(array -> array)
 					.map(JsonElement::getAsJsonObject)
-					.map(jsonAtom -> gson.fromJson(jsonAtom, atomQuery.getAtomClass()))
+					.map(jsonAtom -> RadixJson.getGson().fromJson(jsonAtom, atomQuery.getAtomClass()))
 					.map(atom -> {
 						atom.putDebug("RECEIVED", System.currentTimeMillis());
 						return atom;
@@ -129,7 +171,7 @@ public class RadixJsonRpcClient {
 					.filter(msg -> msg.get("id").getAsString().equals(uuid))
 					.firstOrError()
 					.doOnSubscribe(disposable -> {
-						boolean sendSuccess = wsClient.send(gson.toJson(requestObject));
+						boolean sendSuccess = wsClient.send(RadixJson.getGson().toJson(requestObject));
 						if (!sendSuccess) {
 							disposable.dispose();
 							emitter.onError(new RuntimeException("Could not connect."));
@@ -157,16 +199,24 @@ public class RadixJsonRpcClient {
 					JsonObject cancelParams = new JsonObject();
 					cancelParams.addProperty("subscriberId", uuid);
 					cancelObject.add("params", cancelParams);
-					wsClient.send(gson.toJson(cancelObject));
+					wsClient.send(RadixJson.getGson().toJson(cancelObject));
 				});
 			})
 		);
 	}
 
+	/**
+	 * Attempt to submit an atom to a node. Returns the status of the atom as it
+	 * gets stored on the node.
+	 *
+	 * @param atom the atom to submit
+	 * @param <T> the type of atom
+	 * @return observable of the atom as it gets stored
+	 */
 	public <T extends Atom> Observable<AtomSubmissionUpdate> submitAtom(T atom) {
 		return this.wsClient.connect().andThen(
 			Observable.<AtomSubmissionUpdate>create(emitter -> {
-				JsonElement jsonAtom = gson.toJsonTree(atom, Atom.class);
+				JsonElement jsonAtom = RadixJson.getGson().toJsonTree(atom, Atom.class);
 
 				final String uuid = UUID.randomUUID().toString();
 				JsonObject requestObject = new JsonObject();
@@ -204,7 +254,7 @@ public class RadixJsonRpcClient {
 					.filter(msg -> msg.get("id").getAsString().equals(uuid))
 					.firstOrError()
 					.doOnSubscribe(disposable -> {
-						boolean sendSuccess = wsClient.send(gson.toJson(requestObject));
+						boolean sendSuccess = wsClient.send(RadixJson.getGson().toJson(requestObject));
 						if (!sendSuccess) {
 							disposable.dispose();
 							emitter.onError(new RuntimeException("Could not connect."));
