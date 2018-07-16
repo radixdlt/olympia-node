48291651547d86f31a9520ae30230b2d2b230625
Adam Nybäck <adam@anyro.se>
2018-07-16 17:27:16 +0200

    Minor

--- radixdlt-java/src/test/java/com/radixdlt/client/assets/AssetAmountTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/assets/AssetAmountTest.java
@@ -29,7 +29,7 @@ public class AssetAmountTest {
 	@Test
 	public void testUnusualSubUnits() {
 		// 1 foot = 12 inches
-		final Asset foot = new Asset("FOOT", 12, new EUID(BigInteger.valueOf("TEST".hashCode())));
+		final Asset foot = new Asset("FOOT", 12, new EUID(BigInteger.valueOf("FOOT".hashCode())));
 		assertEquals("0 FOOT", new AssetAmount(foot, 0).toString());
 		assertEquals("1/12 FOOT", new AssetAmount(foot, 1).toString());
 		assertEquals("6/12 FOOT", new AssetAmount(foot, 6).toString());
