807fa12d31274b6d7836bb3e7a738865e23cf9cc
Joshua Primero <josh@radixdlt.com>
2018-07-17 22:53:57 -0400

    Add Simple CreateReply test for Messages

---
+++ radixdlt-java/src/test/java/com/radixdlt/client/messaging/RadixMessageTest.java
@@ -0,0 +1,24 @@
+package com.radixdlt.client.messaging;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import com.radixdlt.client.core.address.RadixAddress;
+import com.radixdlt.client.core.atoms.Atom;
+import org.junit.Test;
+
+public class RadixMessageTest {
+	@Test
+	public void createReplyTest() {
+		RadixAddress to = mock(RadixAddress.class);
+		RadixAddress from = mock(RadixAddress.class);
+		RadixMessageContent content = new RadixMessageContent(to, from, "Hello");
+		Atom atom = mock(Atom.class);
+
+		RadixMessage radixMessage = new RadixMessage(content, atom);
+		RadixMessageContent reply = radixMessage.createReply("Hi");
+		assertTrue(reply.getFrom() == radixMessage.getTo());
+		assertTrue(reply.getTo() == radixMessage.getFrom());
+		assertEquals("Hi", reply.getContent());
+	}
+}
