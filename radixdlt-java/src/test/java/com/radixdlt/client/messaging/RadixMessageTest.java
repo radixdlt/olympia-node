137f708891253c4c48969302702260d88d63b262
Joshua Primero <josh@radixdlt.com>
2018-07-19 19:10:59 +0300

    Fix imports to satisfy sonarqube

--- radixdlt-java/src/test/java/com/radixdlt/client/messaging/RadixMessageTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/messaging/RadixMessageTest.java
@@ -1,6 +1,7 @@
 package com.radixdlt.client.messaging;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import com.radixdlt.client.core.address.RadixAddress;
