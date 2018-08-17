2c2c552d16cb2977945443f918292940efa2613b
Joshua Primero <josh@radixdlt.com>
2018-08-17 12:37:14 -0400

    Increase Coverage of RadixApplicationAPI Junit Test

--- radixdlt-java/src/test/java/com/radixdlt/client/application/RadixApplicationAPITest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/application/RadixApplicationAPITest.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.radixdlt.client.application.RadixApplicationAPI.Result;
 import com.radixdlt.client.application.objects.EncryptedData;
 import com.radixdlt.client.core.address.RadixAddress;
 import com.radixdlt.client.core.atoms.ApplicationPayloadAtom;
@@ -27,32 +28,69 @@ import java.util.function.Supplier;
 import org.junit.Test;
 
 public class RadixApplicationAPITest {
-
-	@Test
-	public void testStoreData() {
+	private RadixApplicationAPI createMockedAPIWhichAlwaysSucceeds() {
 		RadixIdentity identity = mock(RadixIdentity.class);
 		RadixLedger ledger = mock(RadixLedger.class);
-		RadixAddress address = mock(RadixAddress.class);
-		EncryptedData encryptedData = mock(EncryptedData.class);
 		AtomBuilder atomBuilder = mock(AtomBuilder.class);
 		when(atomBuilder.type(any())).thenReturn(atomBuilder);
 		when(atomBuilder.protectors(any())).thenReturn(atomBuilder);
 		when(atomBuilder.payload(any(byte[].class))).thenReturn(atomBuilder);
 		Atom atom = mock(Atom.class);
 		when(identity.sign(any())).thenReturn(Single.just(atom));
-		AtomSubmissionUpdate update = mock(AtomSubmissionUpdate.class);
-		when(update.isComplete()).thenCallRealMethod();
-		when(update.getState()).thenReturn(AtomSubmissionState.SUBMITTING, AtomSubmissionState.SUBMITTED, AtomSubmissionState.STORED);
-		when(ledger.submitAtom(any())).thenReturn(Observable.just(update, update, update));
+
+		AtomSubmissionUpdate submitting = mock(AtomSubmissionUpdate.class);
+		AtomSubmissionUpdate submitted = mock(AtomSubmissionUpdate.class);
+		AtomSubmissionUpdate stored = mock(AtomSubmissionUpdate.class);
+		when(submitting.isComplete()).thenCallRealMethod();
+		when(submitted.isComplete()).thenCallRealMethod();
+		when(stored.isComplete()).thenCallRealMethod();
+
+		when(submitting.getState()).thenReturn(AtomSubmissionState.SUBMITTING);
+		when(submitted.getState()).thenReturn(AtomSubmissionState.SUBMITTED);
+		when(stored.getState()).thenReturn(AtomSubmissionState.STORED);
+
+		when(ledger.submitAtom(any())).thenReturn(Observable.just(submitting, submitted, stored));
 		Supplier<AtomBuilder> atomBuilderSupplier = () -> atomBuilder;
 		UnsignedAtom unsignedAtom = mock(UnsignedAtom.class);
 		when(atomBuilder.buildWithPOWFee(anyInt(), any())).thenReturn(unsignedAtom);
 
-		RadixApplicationAPI api = RadixApplicationAPI.create(identity, ledger, atomBuilderSupplier);
-		TestObserver testObserver = TestObserver.create();
-		api.storeData(encryptedData, address).toCompletable().subscribe(testObserver);
-		testObserver.assertNoErrors();
-		testObserver.assertComplete();
+		return RadixApplicationAPI.create(identity, ledger, atomBuilderSupplier);
+	}
+
+	private void validateSuccessfulStoreDataResult(Result result) {
+		TestObserver completionObserver = TestObserver.create();
+		TestObserver<AtomSubmissionUpdate> updatesObserver = TestObserver.create();
+		result.toCompletable().subscribe(completionObserver);
+		completionObserver.assertNoErrors();
+		completionObserver.assertComplete();
+
+		result.toObservable().subscribe(updatesObserver);
+		updatesObserver.assertNoErrors();
+		updatesObserver.assertComplete();
+		updatesObserver.assertValueCount(3);
+		updatesObserver.assertValueAt(0, atomUpdate -> atomUpdate.getState().equals(AtomSubmissionState.SUBMITTING));
+		updatesObserver.assertValueAt(1, atomUpdate -> atomUpdate.getState().equals(AtomSubmissionState.SUBMITTED));
+		updatesObserver.assertValueAt(2, atomUpdate -> atomUpdate.getState().equals(AtomSubmissionState.STORED));
+	}
+
+	@Test
+	public void testStoreData() {
+		RadixApplicationAPI api = createMockedAPIWhichAlwaysSucceeds();
+		RadixAddress address = mock(RadixAddress.class);
+
+		EncryptedData encryptedData = mock(EncryptedData.class);
+		Result result = api.storeData(encryptedData, address);
+		validateSuccessfulStoreDataResult(result);
+	}
+
+	@Test
+	public void testStoreData2() {
+		RadixApplicationAPI api = createMockedAPIWhichAlwaysSucceeds();
+		RadixAddress address = mock(RadixAddress.class);
+
+		EncryptedData encryptedData = mock(EncryptedData.class);
+		Result result = api.storeData(encryptedData, address, address);
+		validateSuccessfulStoreDataResult(result);
 	}
 
 	@Test
