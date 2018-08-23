211ca2a743afb7b02625df6e356d69b7857dca9e
Joshua Primero <josh@radixdlt.com>
2018-08-23 09:58:30 -0400

    Fix checkstyle

--- radixdlt-java/src/main/java/com/radixdlt/client/core/atoms/Atom.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/core/atoms/Atom.java
@@ -201,6 +201,7 @@ public final class Atom {
 
 	@Override
 	public String toString() {
-		return "Atom hid(" + getHid().toString() + ") destinations(" + destinations + ") particles(" + (particles == null ? 0 : particles.size()) + ")";
+		return "Atom hid(" + getHid().toString() + ") destinations(" + destinations
+			+ ") particles(" + (particles == null ? 0 : particles.size()) + ")";
 	}
 }
