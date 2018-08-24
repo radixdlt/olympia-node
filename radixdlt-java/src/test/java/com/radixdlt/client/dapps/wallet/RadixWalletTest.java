2f4cc4f07d8c7d8ff4f54d9ca245858214214490
Joshua Primero <josh@radixdlt.com>
2018-08-24 18:02:23 -0400

    Add Documentation and another method in Radix Wallet

---
+++ radixdlt-java/src/test/java/com/radixdlt/client/dapps/wallet/RadixWalletTest.java
@@ -0,0 +1,26 @@
+package com.radixdlt.client.dapps.wallet;
+
+import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import com.radixdlt.client.application.RadixApplicationAPI;
+import org.junit.Test;
+
+public class RadixWalletTest {
+	@Test
+	public void nullTest() {
+		RadixApplicationAPI api = mock(RadixApplicationAPI.class);
+		RadixWallet radixWallet = new RadixWallet(api);
+		assertThatThrownBy(() -> radixWallet.getXRDSubUnitBalance(null))
+			.isInstanceOf(NullPointerException.class);
+		assertThatThrownBy(() -> radixWallet.getXRDTransactions(null))
+			.isInstanceOf(NullPointerException.class);
+		assertThatThrownBy(() -> radixWallet.transferXRD(1, null))
+			.isInstanceOf(NullPointerException.class);
+		assertThatThrownBy(() -> radixWallet.transferXRD(1, null, "hi"))
+			.isInstanceOf(NullPointerException.class);
+		assertThatThrownBy(() -> radixWallet.transferXRDWhenAvailable(1, null))
+			.isInstanceOf(NullPointerException.class);
+		assertThatThrownBy(() -> radixWallet.transferXRDWhenAvailable(1, null, "hi"))
+			.isInstanceOf(NullPointerException.class);
+	}
+}
