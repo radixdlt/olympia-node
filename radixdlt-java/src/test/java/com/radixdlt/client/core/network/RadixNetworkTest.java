9e78b799353d5e3f80dfcc1a7cd284c9edc4ff7d
Joshua Primero <josh@radixdlt.com>
2018-09-08 23:50:20 -0400

    Implement API Version Check with Server

--- radixdlt-java/src/test/java/com/radixdlt/client/core/network/RadixNetworkTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/core/network/RadixNetworkTest.java
@@ -1,6 +1,15 @@
 package com.radixdlt.client.core.network;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.radixdlt.client.core.network.WebSocketClient.RadixClientStatus;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+import java.util.NoSuchElementException;
 import java.util.stream.IntStream;
 import org.junit.Test;
 
@@ -8,7 +17,6 @@ public class RadixNetworkTest {
 
 	@Test
 	public void testGetClientsMultipleTimes() {
-
 		RadixNetwork network = new RadixNetwork(() -> Observable.just(
 			new RadixPeer("1", false, 8080),
 			new RadixPeer("2", false, 8080),
@@ -23,5 +31,21 @@ public class RadixNetworkTest {
 				.assertValueAt(1, "http://2:8080/rpc")
 				.assertValueAt(2, "http://3:8080/rpc")
 		);
+	}
+
+	@Test
+	public void testAPIMismatch() {
+		RadixPeer peer = mock(RadixPeer.class);
+		RadixJsonRpcClient client = mock(RadixJsonRpcClient.class);
+		when(peer.servesShards(any())).thenReturn(Maybe.just(peer));
+		when(peer.getRadixClient()).thenReturn(client);
+		when(client.getStatus()).thenReturn(Observable.just(RadixClientStatus.OPEN));
+		when(client.checkAPIVersion()).thenReturn(Single.just(false));
+
+		RadixNetwork network = new RadixNetwork(() -> Observable.just(peer));
+
+		TestObserver<RadixJsonRpcClient> testObserver = TestObserver.create();
+		network.getRadixClient(0L).subscribe(testObserver);
+		testObserver.assertError(NoSuchElementException.class);
 	}
 }
