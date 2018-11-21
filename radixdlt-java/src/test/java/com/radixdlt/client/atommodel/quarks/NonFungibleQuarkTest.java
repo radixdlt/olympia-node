32351b4e650b91b1e9d7bb24ae84e4145999fc77
Joshua Primero <josh@radixdlt.com>
2018-11-20 20:28:42 -0500

    Fix checkstyle

--- radixdlt-java/src/test/java/com/radixdlt/client/atommodel/quarks/NonFungibleQuarkTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/atommodel/quarks/NonFungibleQuarkTest.java
@@ -3,8 +3,6 @@ package com.radixdlt.client.atommodel.quarks;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class NonFungibleQuarkTest {
 	@Test
 	public void testNullConstruction() {
