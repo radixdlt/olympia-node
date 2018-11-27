48e4680420b3fdb924213f40def67912499abd27
Joshua Primero <josh@radixdlt.com>
2018-11-27 17:20:54 -0500

    Add some javadocs

--- radixdlt-java/src/main/java/com/radixdlt/client/application/translate/ActionToParticlesMapper.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/application/translate/ActionToParticlesMapper.java
@@ -3,6 +3,10 @@ package com.radixdlt.client.application.translate;
 import com.radixdlt.client.core.atoms.particles.SpunParticle;
 import io.reactivex.Observable;
 
+/**
+ * Maps a high level application action to lower level spun particles used
+ * to construct an atom.
+ */
 public interface ActionToParticlesMapper {
 	Observable<SpunParticle> map(Action action);
 }
