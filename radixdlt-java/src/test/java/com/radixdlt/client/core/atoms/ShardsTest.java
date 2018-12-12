9cc4232ff7bceae25bee125a15566f8065d019b5
Joshua Primero <josh@radixdlt.com>
2018-12-12 07:07:37 -0500

    Cleanup ShardsTest

--- radixdlt-java/src/test/java/com/radixdlt/client/core/atoms/ShardsTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/core/atoms/ShardsTest.java
@@ -7,29 +7,46 @@ import org.junit.Test;
 
 public class ShardsTest {
 	@Test
-	public void inclusionTest() {
-		Shards shards = Shards.range(1, 1);
-		assertThat(shards.contains(1)).isTrue();
-		assertThat(shards.contains(2)).isFalse();
-		assertThat(shards.intersects(Arrays.asList(1L, 2L, 3L, 4L, 5L))).isTrue();
-		assertThat(shards.intersects(Arrays.asList(2L, 3L, 4L, 5L))).isFalse();
+	public void when_checking_if_included_shard_is_in_singleton_shard_space__intersection_can_be_detected() {
+		long singletonShard = 1;
+		Shards shards = Shards.range(singletonShard, singletonShard);
+		assertThat(shards.contains(singletonShard)).isTrue();
+		assertThat(shards.contains(singletonShard + 1)).isFalse();
+		assertThat(shards.intersects(
+			Arrays.asList(singletonShard, singletonShard + 1, singletonShard + 2, singletonShard + 3, singletonShard + 4))
+		).isTrue();
+		assertThat(shards.intersects(
+			Arrays.asList(singletonShard + 1, singletonShard + 2, singletonShard + 3, singletonShard + 4))
+		).isFalse();
 	}
 
 	@Test
-	public void inclusionNonWrappedTest() {
-		Shards shards = Shards.range(0, 2);
-		assertThat(shards.contains(1)).isTrue();
-		assertThat(shards.contains(3)).isFalse();
-		assertThat(shards.intersects(Arrays.asList(1L, 2L, 3L, 4L, 5L))).isTrue();
-		assertThat(shards.intersects(Arrays.asList(3L, 4L, 5L))).isFalse();
+	public void when_checking_if_included_shard_is_in_non_wrapped_shard_space__intersection_can_be_detected() {
+		long lowShard = 0;
+		long highShard = 2;
+		Shards shards = Shards.range(lowShard, highShard);
+		assertThat(shards.contains(lowShard + 1)).isTrue();
+		assertThat(shards.contains(highShard + 1)).isFalse();
+		assertThat(shards.intersects(
+			Arrays.asList(lowShard, lowShard + 1, lowShard + 2, lowShard + 3, lowShard + 4))
+		).isTrue();
+		assertThat(shards.intersects(
+			Arrays.asList(highShard, highShard + 1, highShard + 2, highShard + 3, highShard + 4))
+		).isTrue();
 	}
 
 	@Test
-	public void inclusionWrappedTest() {
-		Shards shards = Shards.range(Long.MAX_VALUE, 1);
-		assertThat(shards.contains(0)).isTrue();
-		assertThat(shards.contains(2)).isFalse();
-		assertThat(shards.intersects(Arrays.asList(0L, 2L, 3L, 4L, 5L))).isTrue();
-		assertThat(shards.intersects(Arrays.asList(3L, 4L, 5L))).isFalse();
+	public void when_checking_if_included_shard_is_in_wrapped_shard_space__intersection_can_be_detected() {
+		long lowShard = Long.MAX_VALUE;
+		long highShard = 1;
+		Shards shards = Shards.range(lowShard, highShard);
+		assertThat(shards.contains(lowShard + 1)).isTrue();
+		assertThat(shards.contains(highShard + 1)).isFalse();
+		assertThat(shards.intersects(
+			Arrays.asList(lowShard, lowShard + 1, lowShard + 2, lowShard + 3, lowShard + 4))
+		).isTrue();
+		assertThat(shards.intersects(
+			Arrays.asList(highShard, highShard + 1, highShard + 2, highShard + 3, highShard + 4))
+		).isTrue();
 	}
 }
