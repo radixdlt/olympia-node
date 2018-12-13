24a23ccf77893387104c371b9f0d380d48b677c5
Martin Sandiford <martin@radixdlt.com>
2018-12-13 12:24:13 +0000

    Updated after feedback from code review and acceptance criteria review.

--- src/test/java/com/radix/acceptance/create_single_issuance_token_class/CreateSingleIssuanceTokenClass.java
+++ src/test/java/com/radix/acceptance/create_single_issuance_token_class/CreateSingleIssuanceTokenClass.java
@@ -3,6 +3,8 @@ package com.radix.acceptance.create_single_issuance_token_class;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.radix.utils.UInt256;
+
 import com.google.common.collect.Lists;
 import com.radix.acceptance.SpecificProperties;
 import com.radixdlt.client.application.RadixApplicationAPI;
@@ -16,11 +18,11 @@ import com.radixdlt.client.core.RadixUniverse;
 import com.radixdlt.client.core.network.AtomSubmissionUpdate;
 import com.radixdlt.client.core.network.AtomSubmissionUpdate.AtomSubmissionState;
 
-import static com.radixdlt.client.core.network.AtomSubmissionUpdate.AtomSubmissionState.COLLISION;
 import static com.radixdlt.client.core.network.AtomSubmissionUpdate.AtomSubmissionState.STORED;
 import static com.radixdlt.client.core.network.AtomSubmissionUpdate.AtomSubmissionState.SUBMITTED;
 import static com.radixdlt.client.core.network.AtomSubmissionUpdate.AtomSubmissionState.SUBMITTING;
 import static com.radixdlt.client.core.network.AtomSubmissionUpdate.AtomSubmissionState.VALIDATION_ERROR;
+import static org.junit.Assert.assertEquals;
 
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
@@ -35,14 +37,15 @@ import io.reactivex.observers.TestObserver;
  */
 public class CreateSingleIssuanceTokenClass {
 	static {
-		RadixUniverse.bootstrap(Bootstrap.BETANET);
+		if (!RadixUniverse.isInstantiated()) {
+			RadixUniverse.bootstrap(Bootstrap.BETANET);
+		}
 	}
 
 	private static final String NAME = "name";
 	private static final String SYMBOL = "symbol";
 	private static final String DESCRIPTION = "description";
 	private static final String TOTAL_SUPPLY = "totalSupply";
-	private static final String NEW_SUPPLY = "newSupply";
 	private static final String GRANULARITY = "granularity";
 
 	private static final long TIMEOUT_MS = 10_000L; // Timeout in milliseconds
@@ -53,22 +56,11 @@ public class CreateSingleIssuanceTokenClass {
 		NAME,           "RLAU-40 Test token",
 		SYMBOL,			"RLAU",
 		DESCRIPTION,	"RLAU-40 Test token",
-		TOTAL_SUPPLY,	"1000000000",
-		NEW_SUPPLY,		"1000000000",
+		TOTAL_SUPPLY,	scaledToUnscaled(1_000_000_000),
 		GRANULARITY,	"1"
 	);
 	private final List<TestObserver<Object>> observers = Lists.newArrayList();
 	private final List<Disposable> disposables = Lists.newArrayList();
-
-	@Given("^I have access to suitable development tools$")
-	public void i_have_access_to_suitable_development_tools() {
-		// Assumed correct, as this code needs to run somewhere
-	}
-
-	@Given("^I have included the radixdlt-java library$")
-	public void i_have_included_the_radixdlt_java_library() {
-		// Assumed correct, as compile time errors would have occurred
-	}
 
 	@Given("^I have access to a suitable Radix network$")
 	public void i_have_access_to_a_suitable_Radix_network() {
@@ -81,19 +73,33 @@ public class CreateSingleIssuanceTokenClass {
 		this.observers.clear();
 	}
 
-	@When("^I submit a fixed-supply token-creation request with name \"([^\"]*)\", symbol \"([^\"]*)\", totalSupply (\\d+) and granularity (\\d+)$")
-	public void i_submit_a_fixed_supply_token_creation_request_with_name_symbol_totalSupply_and_granularity(
+	@When("^I submit a fixed-supply token-creation request "
+			+ "with name \"([^\"]*)\", symbol \"([^\"]*)\", totalSupply (\\d+) scaled and granularity (\\d+) scaled$")
+	public void i_submit_a_fixed_supply_token_creation_request_with_name_symbol_totalSupply_scaled_and_granularity_scaled(
 			String name, String symbol, int totalSupply, int granularity) {
 		this.properties.put(NAME, name);
 		this.properties.put(SYMBOL, symbol);
-		this.properties.put(TOTAL_SUPPLY, Integer.toString(totalSupply));
-		this.properties.put(GRANULARITY, Integer.toString(granularity));
+		this.properties.put(TOTAL_SUPPLY, scaledToUnscaled(totalSupply));
+		this.properties.put(GRANULARITY, scaledToUnscaled(granularity));
 		createToken(CreateTokenAction.TokenSupplyType.FIXED);
 	}
 
-	@When("^I submit a fixed-supply token-creation request with granularity (\\d+)$")
+	@When("^I submit a fixed-supply token-creation request with symbol \"([^\"]*)\" and totalSupply (\\d+) scaled$")
+	public void i_submit_a_fixed_supply_token_creation_request_with_symbol_totalSupply(String symbol, int totalSupply) {
+		this.properties.put(SYMBOL, symbol);
+		this.properties.put(TOTAL_SUPPLY, scaledToUnscaled(totalSupply));
+		createToken(CreateTokenAction.TokenSupplyType.FIXED);
+	}
+
+	@When("^I submit a fixed-supply token-creation request with granularity (\\d+) scaled$")
 	public void i_submit_a_fixed_supply_token_creation_request_with_granularity(int granularity) {
-		this.properties.put(GRANULARITY, Integer.toString(granularity));
+		this.properties.put(GRANULARITY, scaledToUnscaled(granularity));
+		createToken(CreateTokenAction.TokenSupplyType.FIXED);
+	}
+
+	@When("^I submit a fixed-supply token-creation request with granularity 0$")
+	public void i_submit_a_fixed_supply_token_creation_request_with_granularity_0() {
+		this.properties.put(GRANULARITY, "0");
 		createToken(CreateTokenAction.TokenSupplyType.FIXED);
 	}
 
@@ -103,7 +109,7 @@ public class CreateSingleIssuanceTokenClass {
 		createToken(CreateTokenAction.TokenSupplyType.FIXED);
 	}
 
-	@When("^I submit a token transfer request of (\\d+) for \"([^\"]*)\" to an arbitrary account$")
+	@When("^I submit a token transfer request of (\\d+) scaled for \"([^\"]*)\" to an arbitrary account$")
 	public void i_submit_a_token_transfer_request_of_for_to_an_arbitrary_account(int count, String symbol) {
 		TokenClassReference tokenClass = TokenClassReference.of(api.getMyAddress(), symbol);
 		RadixAddress arbitrary = RadixUniverse.getInstance().getAddressFrom(RadixIdentities.createNew().getPublicKey());
@@ -121,7 +127,7 @@ public class CreateSingleIssuanceTokenClass {
 		this.observers.add(observer);
 	}
 
-	@Then("^I observe the atom being accepted$")
+	@When("^I observe the atom being accepted$")
 	public void i_observe_the_atom_being_accepted() {
 		// "the atom" = most recent atom
 		i_can_observe_atom_being_accepted(observers.size());
@@ -138,11 +144,6 @@ public class CreateSingleIssuanceTokenClass {
 		awaitAtomStatus(atomNumber, STORED);
 	}
 
-	@Then("^I can observe atom (\\d+) being rejected as a collision$")
-	public void i_can_observe_atom_being_rejected_as_a_collision(int atomNumber) {
-		awaitAtomStatus(atomNumber, COLLISION);
-	}
-
 	@Then("^I can observe the atom being rejected with a validation error$")
 	public void i_can_observe_the_atom_being_rejected_as_a_validation_error() {
 		// "the atom" = most recent atom
@@ -152,6 +153,18 @@ public class CreateSingleIssuanceTokenClass {
 	@Then("^I can observe atom (\\d+) being rejected with a validation error$")
 	public void i_can_observe_atom_being_rejected_as_a_validation_error(int atomNumber) {
 		awaitAtomStatus(atomNumber, VALIDATION_ERROR);
+	}
+
+	@Then("^I can observe token \"([^\"]*)\" balance equal to (\\d+) scaled$")
+	public void i_can_observe_token_balance_equal_to_scaled(String symbol, int balance) {
+		TokenClassReference tokenClass = TokenClassReference.of(api.getMyAddress(), symbol);
+		// Ensure balance is up-to-date.
+		BigDecimal tokenBalanceDecimal = api.getBalance(api.getMyAddress(), tokenClass)
+			.firstOrError()
+			.blockingGet();
+		UInt256 tokenBalance = TokenClassReference.unitsToSubunits(tokenBalanceDecimal);
+		UInt256 requiredBalance = TokenClassReference.unitsToSubunits(balance);
+		assertEquals(requiredBalance, tokenBalance);
 	}
 
 	@After
@@ -166,8 +179,8 @@ public class CreateSingleIssuanceTokenClass {
 				this.properties.get(NAME),
 				this.properties.get(SYMBOL),
 				this.properties.get(DESCRIPTION),
-				TokenClassReference.unitsToSubunits(Long.valueOf(this.properties.get(TOTAL_SUPPLY))),
-				TokenClassReference.unitsToSubunits(Long.valueOf(this.properties.get(GRANULARITY))),
+				UInt256.from(this.properties.get(TOTAL_SUPPLY)),
+				UInt256.from(this.properties.get(GRANULARITY)),
 				tokenCreateSupplyType)
 			.toObservable()
 			.doOnNext(System.out::println)
@@ -182,5 +195,9 @@ public class CreateSingleIssuanceTokenClass {
 			.assertNoErrors()
 			.assertNoTimeout()
 			.assertValues(SUBMITTING, SUBMITTED, finalState);
+	}
+
+	private static String scaledToUnscaled(int scaled) {
+		return TokenClassReference.unitsToSubunits(scaled).toString();
 	}
 }
--- src/test/resources/com/radix/acceptance/create_single_issuance_token_class/CreateSingleIssuanceTokenClass.feature
+++ src/test/resources/com/radix/acceptance/create_single_issuance_token_class/CreateSingleIssuanceTokenClass.feature
@@ -6,56 +6,48 @@ Feature: Create Single Issuance Token Class
   So that I can easily create a token coming from an ethereum background without applying too much extra learning
 
   Scenario: 1: Creating a unique token
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
-    When I submit a fixed-supply token-creation request with name "RLAU Test", symbol "RLAU", totalSupply 1000000000 and granularity 1
+    Given I have access to a suitable Radix network
+    When I submit a fixed-supply token-creation request with name "RLAU Test", symbol "RLAU", totalSupply 100000000 scaled and granularity 1 scaled
     Then I can observe the atom being accepted
 
   Scenario: 2: Creating a conflicting token
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
+    Given I have access to a suitable Radix network
     When I submit a fixed-supply token-creation request with symbol "RLAU"
+    And I observe the atom being accepted
     And I submit a fixed-supply token-creation request with symbol "RLAU"
-    Then I can observe atom 1 being accepted
-    And I can observe atom 2 being rejected as a collision
+    Then I can observe atom 2 being rejected with a validation error
 
   Scenario: 3: Creating a token with disallowed granularity
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
+    Given I have access to a suitable Radix network
     When I submit a fixed-supply token-creation request with granularity 0
     Then I can observe the atom being rejected with a validation error
 
   Scenario: 4: Transacting within granularity
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
-    When I submit a fixed-supply token-creation request with granularity 2
+    Given I have access to a suitable Radix network
+    When I submit a fixed-supply token-creation request with granularity 2 scaled
     And I observe the atom being accepted
-    And I submit a token transfer request of 100 for "RLAU" to an arbitrary account
+    And I submit a token transfer request of 100 scaled for "RLAU" to an arbitrary account
     Then I can observe atom 2 being accepted
 
   Scenario: 5: Transacting outside granularity
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
-    When I submit a fixed-supply token-creation request with granularity 2
+    Given I have access to a suitable Radix network
+    When I submit a fixed-supply token-creation request with granularity 2 scaled
     And I observe the atom being accepted
-    And I submit a token transfer request of 1 for "RLAU" to an arbitrary account
+    And I submit a token transfer request of 1 scaled for "RLAU" to an arbitrary account
     Then I can observe atom 2 being rejected with a validation error
 
   Scenario: 6: Token symbol too short
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
+    Given I have access to a suitable Radix network
     When I submit a fixed-supply token-creation request with symbol "1"
     Then I can observe the atom being rejected with a validation error
 
   Scenario: 7: Token symbol too long
-    Given I have access to suitable development tools
-    And I have included the radixdlt-java library
-    And I have access to a suitable Radix network
+    Given I have access to a suitable Radix network
     When I submit a fixed-supply token-creation request with symbol "123456"
     Then I can observe the atom being rejected with a validation error
+
+  Scenario: 8: Total supply
+    Given I have access to a suitable Radix network
+    When I submit a fixed-supply token-creation request with symbol "RLAU" and totalSupply 1000 scaled
+    Then I can observe the atom being accepted
+    And I can observe token "RLAU" balance equal to 1000 scaled
