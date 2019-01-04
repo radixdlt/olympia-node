f2fe22ff4db21a0768035a676b2819602395abaa
Martin Sandiford <martin@radixdlt.com>
2019-01-04 18:11:10 +0000

    Added Javadoc to UnknownTokenException as part of review feedback.

--- radixdlt-java/src/main/java/com/radixdlt/client/application/translate/tokens/UnknownTokenException.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/application/translate/tokens/UnknownTokenException.java
@@ -1,9 +1,19 @@
 package com.radixdlt.client.application.translate.tokens;
 
+/**
+ * Exception thrown when information on an unknown token is requested.
+ */
 public class UnknownTokenException extends RuntimeException {
 	private static final long serialVersionUID = -1684509326376059175L;
 	private final TokenClassReference tokenClassReference;
 
+    /**
+     * Constructs a new unknown token exception with the specified token class reference.
+     *
+     * @param tokenClassReference the token class that could not be found.
+     *     Note that {@link #getMessage()} will include the token class name
+     *     in the exception detail message.
+     */
 	public UnknownTokenException(TokenClassReference tokenClassReference) {
 		super("Unknown token: " + tokenClassReference);
 		this.tokenClassReference = tokenClassReference;
