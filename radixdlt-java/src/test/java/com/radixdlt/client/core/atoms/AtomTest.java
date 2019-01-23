2a56eb61b53305e3c8fae85d0f4c3d08cfe771c0
Alexander Cyon <alex@radixdlt.com>
2019-01-23 10:20:55 +0000

    Added comparison of Dson hex string in empty atom test

--- radixdlt-java/src/test/java/com/radixdlt/client/core/atoms/AtomTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/core/atoms/AtomTest.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import com.radixdlt.client.core.atoms.particles.Spin;
+import com.radixdlt.client.core.util.Hash;
+import okio.ByteString;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 
 public class AtomTest {
@@ -16,6 +19,7 @@ public class AtomTest {
 		assertTrue(atom.getDataParticles().isEmpty());
 		assertTrue(atom.getConsumables(Spin.UP).isEmpty());
 		assertTrue(atom.getConsumables(Spin.DOWN).isEmpty());
+		assertEquals("bf6a73657269616c697a65721a001ed1516776657273696f6e1864ff", ByteString.of(atom.toDson()).hex());
 		assertEquals("1b1cff72cb4f79d2eb50b5fb2777d65bebb5cad146e2006f25cde7a53445ffe7", atom.getHash().toHexString());
 		assertEquals("1b1cff72cb4f79d2eb50b5fb2777d65b", atom.getHid().toHexString());
 		assertEquals(new Long(0), atom.getTimestamp());
