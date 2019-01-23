1fa4d25745c73f66d9176c4932f4b08497bc127d
Alexander Cyon <alex@radixdlt.com>
2019-01-23 14:05:23 +0000

    Adding documentation and simplifying HashTest using StandardCharsets

--- radixdlt-java/src/test/java/com/radixdlt/client/core/atoms/AtomTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/core/atoms/AtomTest.java
@@ -19,6 +19,8 @@ public class AtomTest {
 		assertTrue(atom.getDataParticles().isEmpty());
 		assertTrue(atom.getConsumables(Spin.UP).isEmpty());
 		assertTrue(atom.getConsumables(Spin.DOWN).isEmpty());
+		/// The origin of these hashes are this library it self, commit: acbc5307cf5c9f7e1c30300f7438ef5dbc3bb629
+		/// These hashes can be used as a reference for other Radix libraries, e.g. Swift.
 		assertEquals("bf6a73657269616c697a65721a001ed1516776657273696f6e1864ff", ByteString.of(atom.toDson()).hex());
 		assertEquals("1b1cff72cb4f79d2eb50b5fb2777d65bebb5cad146e2006f25cde7a53445ffe7", atom.getHash().toHexString());
 		assertEquals("1b1cff72cb4f79d2eb50b5fb2777d65b", atom.getHid().toHexString());
--- radixdlt-java/src/test/java/com/radixdlt/client/util/HashTest.java
+++ radixdlt-java/src/test/java/com/radixdlt/client/util/HashTest.java
@@ -4,22 +4,21 @@ import com.radixdlt.client.core.util.Hash;
 import okio.ByteString;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class HashTest {
 
     @Test
-    public void testDoubleSha256Hash() {
-        byte[] data = new byte[0];
-        try {
-            data = "Hello Radix".getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+    public void test_sha256_hash_as_reference_for_other_libraries()  {
+        byte[] data = "Hello Radix".getBytes(StandardCharsets.UTF_8);
         byte[] singleHash = Hash.sha256(data);
         byte[] doubleHash = Hash.sha256(singleHash);
+
+        // These hashes as just the result of running the sha256 once and output the values
+        // These are then used as reference for other libraries, especially Swift which
+        // lacks native Sha256 methods.
         assertEquals("374d9dc94c1252acf828cdfb94946cf808cb112aa9760a2e6216c14b4891f934", ByteString.of(singleHash).hex());
         assertEquals("fd6be8b4b12276857ac1b63594bf38c01327bd6e8ae0eb4b0c6e253563cc8cc7", ByteString.of(doubleHash).hex());
     }
