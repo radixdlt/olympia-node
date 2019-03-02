ef6b376dfe60242ba3544bd9243e7e4a6a53551a
Florian Cäsar <florian.caesar@outlook.com>
2019-03-02 22:13:34 +0100

    Add Minted, Transferred, BurnedTokens particles
    currently unused, intermediate commit

---
+++ radixdlt-java/src/main/java/com/radixdlt/client/atommodel/tokens/BurnedTokensParticle.java
@@ -0,0 +1,157 @@
+package com.radixdlt.client.atommodel.tokens;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.radixdlt.client.application.translate.tokens.TokenClassReference;
+import com.radixdlt.client.atommodel.Accountable;
+import com.radixdlt.client.atommodel.Fungible;
+import com.radixdlt.client.atommodel.FungibleType;
+import com.radixdlt.client.atommodel.Ownable;
+import com.radixdlt.client.atommodel.accounts.RadixAddress;
+import com.radixdlt.client.core.atoms.RadixHash;
+import com.radixdlt.client.core.atoms.particles.Particle;
+import com.radixdlt.client.core.atoms.particles.RadixResourceIdentifer;
+import com.radixdlt.client.core.crypto.ECKeyPair;
+import com.radixdlt.client.core.crypto.ECPublicKey;
+import org.radix.serialization2.DsonOutput;
+import org.radix.serialization2.DsonOutput.Output;
+import org.radix.serialization2.SerializerId2;
+import org.radix.serialization2.client.Serialize;
+import org.radix.utils.UInt256;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ *  A particle which represents an amount of fungible tokens owned by some key owner and stored in an account.
+ */
+@SerializerId2("BURNEDTOKENSPARTICLE")
+public class BurnedTokensParticle extends Particle implements Accountable, Ownable, Fungible {
+	@JsonProperty("address")
+	@DsonOutput(Output.ALL)
+	private RadixAddress address;
+
+	@JsonProperty("tokenTypeReference")
+	@DsonOutput(Output.ALL)
+	private RadixResourceIdentifer tokenTypeReference;
+
+	@JsonProperty("granularity")
+	@DsonOutput(Output.ALL)
+	private UInt256 granularity;
+
+	@JsonProperty("planck")
+	@DsonOutput(Output.ALL)
+	private long planck;
+
+	@JsonProperty("nonce")
+	@DsonOutput(Output.ALL)
+	private long nonce;
+
+	@JsonProperty("amount")
+	@DsonOutput(Output.ALL)
+	private UInt256 amount;
+
+	protected BurnedTokensParticle() {
+	}
+
+	public BurnedTokensParticle(UInt256 amount, UInt256 granularity, RadixAddress address, long nonce,
+	                            TokenClassReference tokenTypeReference, long planck) {
+		super();
+
+		// Redundant null check added for completeness
+		Objects.requireNonNull(amount, "amount is required");
+		if (amount.isZero()) {
+			throw new IllegalArgumentException("Amount is zero");
+		}
+
+		this.address = address;
+		this.tokenTypeReference = new RadixResourceIdentifer(tokenTypeReference.getAddress(), "tokenclasses", tokenTypeReference.getSymbol());
+		this.granularity = granularity;
+		this.planck = planck;
+		this.nonce = nonce;
+		this.amount = amount;
+	}
+
+	@Override
+	public Set<RadixAddress> getAddresses() {
+		return Collections.singleton(address);
+	}
+
+	public RadixAddress getAddress() {
+		return address;
+	}
+
+	public void addConsumerQuantities(UInt256 amount, ECKeyPair newOwner, Map<ECKeyPair, UInt256> consumerQuantities) {
+		if (amount.compareTo(getAmount()) > 0) {
+			throw new IllegalArgumentException(
+				"Unable to create consumable with amount " + amount + " (available: " + getAmount() + ")"
+			);
+		}
+
+		if (amount.equals(getAmount())) {
+			consumerQuantities.merge(newOwner, amount, UInt256::add);
+			return;
+		}
+
+		consumerQuantities.merge(newOwner, amount, UInt256::add);
+		consumerQuantities.merge(getAddress().toECKeyPair(), getAmount().subtract(amount), UInt256::add);
+	}
+
+	@Override
+	public FungibleType getType() {
+		return FungibleType.BURNED;
+	}
+
+	@Override
+	public long getPlanck() {
+		return this.planck;
+	}
+
+	@Override
+	public long getNonce() {
+		return this.nonce;
+	}
+
+	public TokenClassReference getTokenTypeReference() {
+		return TokenClassReference.of(tokenTypeReference.getAddress(), tokenTypeReference.getUnique());
+	}
+
+	@Override
+	public UInt256 getAmount() {
+		return this.amount;
+	}
+
+	public UInt256 getGranularity() {
+		return this.granularity;
+	}
+
+	public Set<ECPublicKey> getOwnersPublicKeys() {
+		return Collections.singleton(this.address.getPublicKey());
+	}
+
+	@Override
+	public ECPublicKey getOwner() {
+		return this.address.getPublicKey();
+	}
+
+	public RadixHash getHash() {
+		return RadixHash.of(getDson());
+	}
+
+	public byte[] getDson() {
+		return Serialize.getInstance().toDson(this, Output.HASH);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s[%s:%s:%s:%s:%s:%s]",
+			getClass().getSimpleName(),
+			String.valueOf(tokenTypeReference),
+			String.valueOf(amount),
+			String.valueOf(granularity),
+			String.valueOf(address),
+			planck,
+			nonce);
+	}
+}
