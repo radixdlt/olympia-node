1e040fd588a47d70b9627bb9b104bd59f295be3e
Florian Cäsar <florian.caesar@outlook.com>
2019-03-08 07:41:01 +0100

    Add regression tests for overmint across token types

---
+++ src/test/java/com/radix/regression/MintTooManyTokensOfDifferentTypesTest.java
@@ -0,0 +1,58 @@
+package com.radix.regression;
+
+import com.radixdlt.client.application.RadixApplicationAPI;
+import com.radixdlt.client.application.identity.RadixIdentities;
+import com.radixdlt.client.application.identity.RadixIdentity;
+import com.radixdlt.client.application.translate.tokenclasses.CreateTokenAction;
+import com.radixdlt.client.core.Bootstrap;
+import com.radixdlt.client.core.RadixUniverse;
+import com.radixdlt.client.core.network.actions.SubmitAtomAction;
+import io.reactivex.observers.BaseTestConsumer;
+import io.reactivex.observers.BaseTestConsumer.TestWaitStrategy;
+import io.reactivex.observers.TestObserver;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.radix.utils.UInt256;
+
+public class MintTooManyTokensOfDifferentTypesTest {
+	@BeforeClass
+	public static void setup() {
+		if (!RadixUniverse.isInstantiated()) {
+			RadixUniverse.bootstrap(Bootstrap.BETANET);
+		}
+	}
+
+	@Test
+	public void given_a_token_with_max_supply_created_in_one_account__when_another_token_with_max_supply_is_created_in_another_account__then_the_client_should_be_notified_of_success() {
+		RadixApplicationAPI api0 = RadixApplicationAPI.create(RadixIdentities.createNew());
+		RadixApplicationAPI api1 = RadixApplicationAPI.create(RadixIdentities.createNew());
+
+		createToken(api0)
+			.awaitCount(4, TestWaitStrategy.SLEEP_100MS, 10000)
+			.assertSubscribed()
+			.assertNoTimeout()
+			.assertNoErrors();
+
+		createToken(api1)
+			.awaitCount(4, TestWaitStrategy.SLEEP_100MS, 10000)
+			.assertSubscribed()
+			.assertNoTimeout()
+			.assertNoErrors();
+	}
+
+	private static TestObserver createToken(RadixApplicationAPI api) {
+		TestObserver<SubmitAtomAction> observer = new TestObserver<>();
+		api.createToken(
+			"TestToken",
+			"TEST",
+			"TestToken",
+			UInt256.MAX_VALUE,
+			UInt256.ONE,
+			CreateTokenAction.TokenSupplyType.MUTABLE)
+			.toObservable()
+			.doOnNext(System.out::println)
+			.subscribe(observer);
+
+		return observer;
+	}
+}
