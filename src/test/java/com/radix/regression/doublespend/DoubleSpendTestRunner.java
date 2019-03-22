0aa1730a314f3a329d5ffc0e21dba8d257973b5b
Joshua Primero <josh@radixdlt.com>
2019-03-22 22:15:43 +0800

    Test postConsensusCondition on all apis being used

--- src/test/java/com/radix/regression/doublespend/DoubleSpendTestRunner.java
+++ src/test/java/com/radix/regression/doublespend/DoubleSpendTestRunner.java
@@ -6,6 +6,7 @@ import com.radix.regression.Util;
 import com.radixdlt.client.application.RadixApplicationAPI;
 import com.radixdlt.client.application.RadixApplicationAPI.Result;
 import com.radixdlt.client.application.identity.RadixIdentities;
+import com.radixdlt.client.application.identity.RadixIdentity;
 import com.radixdlt.client.application.translate.Action;
 import com.radixdlt.client.application.translate.ApplicationState;
 import com.radixdlt.client.application.translate.ShardedAppStateId;
@@ -55,7 +56,29 @@ public final class DoubleSpendTestRunner {
 			});
 	}
 
-	public void execute() {
+	private static class SingleNodeAPI {
+		private final RadixApplicationAPI api;
+		private final RadixNode node;
+
+		public SingleNodeAPI(RadixNode node, RadixIdentity identity) {
+			this.node = node;
+			this.api = RadixApplicationAPI.create(
+				new BootstrapConfig() {
+				    @Override
+				    public RadixUniverseConfig getConfig() {
+					    return RadixUniverseConfigs.getBetanet();
+				    }
+
+				    @Override
+				    public List<RadixNetworkEpic> getDiscoveryEpics() {
+					    return Collections.singletonList(new DiscoverSingleNodeEpic(node, RadixUniverseConfigs.getBetanet()));
+				    }
+			    },
+				identity);
+		}
+	}
+
+	void execute() {
 		RadixApplicationAPI api = RadixApplicationAPI.create(Bootstrap.LOCALHOST, RadixIdentities.createNew());
 
 		DoubleSpendTestConfig doubleSpendTestConfig = testSupplier.apply(api);
@@ -99,39 +122,24 @@ public final class DoubleSpendTestRunner {
 					.collect(Collectors.toList())
 			);
 
-		Observable<RadixApplicationAPI> singleNodeApis = Observable.merge(twoNodes.toObservable(), oneNode.toObservable())
+		Observable<SingleNodeAPI> singleNodeApis = Observable.merge(twoNodes.toObservable(), oneNode.toObservable())
 			.firstOrError()
 			.flatMapObservable(l -> l.size() == 1 ? Observable.just(l.get(0), l.get(0)) : Observable.fromIterable(l))
-			.map(node ->
-				RadixApplicationAPI.create(new BootstrapConfig() {
-					@Override
-					public RadixUniverseConfig getConfig() {
-						return RadixUniverseConfigs.getBetanet();
-					}
-
-					@Override
-					public List<RadixNetworkEpic> getDiscoveryEpics() {
-						return Collections.singletonList(new DiscoverSingleNodeEpic(node, RadixUniverseConfigs.getBetanet()));
-					}
-				},
-				api.getMyIdentity())
-			)
+			.map(node -> new SingleNodeAPI(node, api.getMyIdentity()))
 			.cache();
 
 
 		// When the account executes two transfers via two different nodes at the same time
-		Single<List<Pair<RadixApplicationAPI, List<Action>>>> conflictingAtoms =
+		Observable<Pair<SingleNodeAPI, List<Action>>> conflictingAtoms =
 			Observable.zip(
 				singleNodeApis,
 				Observable.fromIterable(doubleSpendTestConfig.conflictingActions()),
 				Pair::of
-			)
-			.toList();
+			);
 
 		TestObserver<SubmitAtomAction> submissionObserver = TestObserver.create(Util.loggingObserver("Submission"));
 		conflictingAtoms
-			.flattenAsObservable(l -> l)
-			.flatMap(a -> Observable.fromIterable(a.getFirst().executeSequentially(a.getSecond()))
+			.flatMap(a -> Observable.fromIterable(a.getFirst().api.executeSequentially(a.getSecond()))
 				.flatMap(Result::toObservable)
 				.takeUntil(s -> {
 					if (s instanceof SubmitAtomResultAction) {
@@ -143,23 +151,29 @@ public final class DoubleSpendTestRunner {
 			)
 			.subscribe(submissionObserver);
 
-		Map<ShardedAppStateId, TestObserver<ApplicationState>> testObservers = doubleSpendTestConfig.postConsensusCondition().getStateRequired().stream()
-			.collect(Collectors.toMap(
-				Pair::getSecond,
-				pair -> {
-					final String name = pair.getFirst();
-					final ShardedAppStateId id = pair.getSecond();
-					final RadixApplicationAPI newApi = RadixApplicationAPI.create(Bootstrap.LOCALHOST, RadixIdentities.createNew());
-					final TestObserver<ApplicationState> testObserver = TestObserver.create(Util.loggingObserver(name));
-					newApi.getState(id.stateClass(), id.address()).subscribe(testObserver);
-					return testObserver;
-				}
-			));
+		List<Map<ShardedAppStateId, TestObserver<ApplicationState>>> testObserversPerApi = singleNodeApis
+			.map(singleNodeApi ->
+				doubleSpendTestConfig.postConsensusCondition().getStateRequired().stream()
+					.collect(Collectors.toMap(
+						Pair::getSecond,
+						pair -> {
+							final String name = pair.getFirst();
+							final ShardedAppStateId id = pair.getSecond();
+							final TestObserver<ApplicationState> testObserver =
+								TestObserver.create(Util.loggingObserver(singleNodeApi.node + " " + name));
+
+							singleNodeApi.api.getState(id.stateClass(), id.address()).subscribe(testObserver);
+							return testObserver;
+						}
+					))
+			)
+			.toList()
+			.blockingGet();
 
 		// Wait for network to resolve conflict
 		TestObserver<RadixNodeAction> lastUpdateObserver = TestObserver.create(Util.loggingObserver("Last Update"));
 		singleNodeApis.flatMap(singleNodeApi ->
-			singleNodeApi.getNetworkController()
+			singleNodeApi.api.getNetworkController()
 			.getActions()
 			.filter(a -> a instanceof FetchAtomsObservationAction || a instanceof SubmitAtomAction)
 		)
@@ -169,16 +183,18 @@ public final class DoubleSpendTestRunner {
 		lastUpdateObserver.awaitTerminalEvent();
 		submissionObserver.awaitTerminalEvent();
 
-		Map<ShardedAppStateId, ApplicationState> state = testObservers.entrySet().stream()
-			.collect(Collectors.toMap(
-				Entry::getKey,
-				e -> {
-					List<ApplicationState> values = e.getValue().values();
-					return values.get(values.size() - 1);
-				}
-			));
-		testObservers.forEach((k,v) -> v.dispose());
+		testObserversPerApi.forEach(testObservers -> {
+			Map<ShardedAppStateId, ApplicationState> state = testObservers.entrySet().stream()
+				.collect(Collectors.toMap(
+					Entry::getKey,
+					e -> {
+						List<ApplicationState> values = e.getValue().values();
+						return values.get(values.size() - 1);
+					}
+				));
+			testObservers.forEach((k,v) -> v.dispose());
 
-		assertThat(state).is(doubleSpendTestConfig.postConsensusCondition().getCondition());
+			assertThat(state).is(doubleSpendTestConfig.postConsensusCondition().getCondition());
+		});
 	}
 }
