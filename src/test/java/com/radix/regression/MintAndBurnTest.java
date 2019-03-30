5e3b8cc199a61af591ec2b6b8b2476303716a66f
Joshua Primero <josh@radixdlt.com>
2019-03-30 18:04:14 +0800

    Fix token supply check

--- src/test/java/com/radix/regression/MintAndBurnTest.java
+++ src/test/java/com/radix/regression/MintAndBurnTest.java
@@ -5,26 +5,29 @@ import com.radixdlt.client.application.RadixApplicationAPI.Result;
 import com.radixdlt.client.application.identity.RadixIdentities;
 import com.radixdlt.client.application.translate.tokens.TokenUnitConversions;
 import com.radixdlt.client.core.Bootstrap;
+import com.radixdlt.client.core.network.actions.SubmitAtomResultAction;
+import com.radixdlt.client.core.network.actions.SubmitAtomResultAction.SubmitAtomResultActionType;
 import org.junit.Test;
 import org.radix.utils.UInt256;
 
 public class MintAndBurnTest {
 	@Test
 	public void given_an_account_owner_who_created_a_token__when_the_owner_mints_max_then_burns_max_then_mints_max__then_it_should_all_be_successful() throws Exception {
-
-		// Given account owner listening to own messages
 		RadixApplicationAPI api = RadixApplicationAPI.create(Bootstrap.LOCALHOST_SINGLENODE, RadixIdentities.createNew());
-		Result result = api.createMultiIssuanceToken("Joshy Token", "JOSH");
-		result.toObservable().subscribe(System.out::println);
-		result.blockUntilComplete();
+		Result result0 = api.createMultiIssuanceToken("Joshy Token", "JOSH", "Best token");
+		result0.toObservable().subscribe(System.out::println);
+		result0.toCompletable().blockingAwait();
 
-		api.mintTokens("JOSH", TokenUnitConversions.subunitsToUnits(UInt256.MAX_VALUE))
-			.blockUntilComplete();
+		Result result1 = api.mintTokens("JOSH", TokenUnitConversions.subunitsToUnits(UInt256.MAX_VALUE));
+		result1.toObservable().subscribe(System.out::println);
+		result1.toCompletable().blockingAwait();
 
-		api.burnTokens("JOSH", TokenUnitConversions.subunitsToUnits(UInt256.MAX_VALUE))
-			.blockUntilComplete();
+		Result result2 = api.burnTokens("JOSH", TokenUnitConversions.subunitsToUnits(UInt256.MAX_VALUE));
+		result2.toObservable().subscribe(System.out::println);
+		result2.toCompletable().blockingAwait();
 
-		api.mintTokens("JOSH", TokenUnitConversions.subunitsToUnits(UInt256.MAX_VALUE))
-			.blockUntilComplete();
+		Result result3 = api.mintTokens("JOSH", TokenUnitConversions.subunitsToUnits(UInt256.MAX_VALUE));
+		result3.toObservable().subscribe(System.out::println);
+		result3.toCompletable().blockingAwait();
 	}
 }
