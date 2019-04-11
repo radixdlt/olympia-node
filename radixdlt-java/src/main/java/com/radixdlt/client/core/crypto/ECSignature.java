ad330acbe69c8f313958a9c5ae29119ae25ec631
Marc Rubio <marc@radixdlt.com>
2019-04-11 12:55:06 +0100

    Add ECSignature constructor which takes bytes as an argument

--- radixdlt-java/src/main/java/com/radixdlt/client/core/crypto/ECSignature.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/core/crypto/ECSignature.java
@@ -1,7 +1,11 @@
 package com.radixdlt.client.core.crypto;
 
+import java.io.IOException;
 import java.math.BigInteger;
 
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.DLSequence;
 import org.bouncycastle.util.encoders.Base64;
 import org.radix.serialization2.DsonOutput;
 import org.radix.serialization2.DsonOutput.Output;
@@ -23,6 +27,21 @@ public class ECSignature extends SerializableObject {
 	public ECSignature(BigInteger r, BigInteger s) {
 		this.r = Bytes.trimLeadingZeros(r.toByteArray());
 		this.s = Bytes.trimLeadingZeros(s.toByteArray());
+	}
+
+	public ECSignature(byte[] bytes) {
+		DLSequence seq;
+		ASN1Integer r, s;
+		try (ASN1InputStream decoder = new ASN1InputStream(bytes)) {
+			seq = (DLSequence) decoder.readObject();
+			r = (ASN1Integer) seq.getObjectAt(0);
+			s = (ASN1Integer) seq.getObjectAt(1);
+		} catch (ClassCastException | IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+
+		this.r = r.getPositiveValue().toByteArray();
+		this.s = s.getPositiveValue().toByteArray();
 	}
 
 	public String getRBase64() {
