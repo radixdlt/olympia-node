f816a4e3eabe5d7ef506b3525647acc3e84cb0b3
Florian Cäsar <florian.caesar@outlook.com>
2019-04-12 12:09:43 +0200

    Rename internal.* -> network.*

--- radixdlt-java/src/main/java/com/radixdlt/client/core/network/jsonrpc/RadixSystem.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/core/network/jsonrpc/RadixSystem.java
@@ -12,7 +12,7 @@ import com.radixdlt.client.core.atoms.Shards;
 
 import static org.radix.serialization2.MapHelper.mapOf;
 
-@SerializerId2("internal.system")
+@SerializerId2("network.system")
 public class RadixSystem extends SerializableObject {
 	private Shards shards;
 
