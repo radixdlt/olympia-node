84d7921da6dccb0b7064917315f99403809f4044
Joshua Primero <josh@radixdlt.com>
2019-04-16 20:55:18 -0400

    Add missing unit test

---
+++ radixdlt-java/src/test/java/com/radixdlt/client/application/translate/unique/UniqueIdTest.java
@@ -0,0 +1,18 @@
+package com.radixdlt.client.application.translate.unique;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+import com.radixdlt.client.atommodel.accounts.RadixAddress;
+import org.junit.Test;
+
+public class UniqueIdTest {
+	@Test
+	public void when_unique_id_is_created_with_null__then_null_pointer_exception_is_thrown() {
+		assertThatThrownBy(() -> new UniqueId(null, "test"))
+			.isInstanceOf(NullPointerException.class);
+
+		assertThatThrownBy(() -> new UniqueId(mock(RadixAddress.class), null))
+			.isInstanceOf(NullPointerException.class);
+	}
+}
