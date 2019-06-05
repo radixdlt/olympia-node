0debcae52288962512dbecda55e2a4c9e72c479d
Joshua Primero <josh@radixdlt.com>
2019-06-05 11:39:12 -0400

    Fix TranslucentAtomTest

--- src/test/java/com/radix/regression/TranslucentAtomTest.java
+++ src/test/java/com/radix/regression/TranslucentAtomTest.java
@@ -1,11 +1,13 @@
 package com.radix.regression;
 
+import com.google.common.collect.ImmutableSet;
 import com.radixdlt.client.application.RadixApplicationAPI;
 import com.radixdlt.client.application.RadixApplicationAPI.Result;
 import com.radixdlt.client.application.identity.LocalRadixIdentity;
 import com.radixdlt.client.application.identity.RadixIdentities;
 import com.radixdlt.client.atommodel.message.MessageParticle;
 import com.radixdlt.client.atommodel.message.MessageParticle.MessageParticleBuilder;
+import com.radixdlt.client.atommodel.message.MetadataMap;
 import com.radixdlt.client.core.Bootstrap;
 import com.radixdlt.client.core.atoms.Atom;
 import com.radixdlt.client.core.atoms.ParticleGroup;
@@ -32,14 +34,12 @@ public class TranslucentAtomTest {
 
 	@Test
 	public void testWrongDestination() {
-		MessageParticle messageParticle = new MessageParticleBuilder()
-			.from(api.getMyAddress())
-			.to(api.getMyAddress())
-			.payload(new byte[1])
-			.nonce(0)
-			.build();
-		messageParticle.getDestinations().removeIf(p -> true);
-		messageParticle.getDestinations().add(EUID.ONE);
+		MessageParticle messageParticle = new MessageParticle(
+			api.getMyAddress(),
+			api.getMyAddress(),
+			new byte[1],
+			new MetadataMap(),
+			0, ImmutableSet.of(EUID.ONE));
 		UnsignedAtom unsignedAtom = new UnsignedAtom(new Atom(ParticleGroup.of(SpunParticle.up(messageParticle)), System.currentTimeMillis()));
 		Atom signedAtom = this.identity.syncSign(unsignedAtom);
 		Result result = api.submitAtom(signedAtom);
@@ -57,13 +57,12 @@ public class TranslucentAtomTest {
 
 	@Test
 	public void testExtraDestination() {
-		MessageParticle messageParticle = new MessageParticleBuilder()
-			.from(api.getMyAddress())
-			.to(api.getMyAddress())
-			.payload(new byte[1])
-			.nonce(0)
-			.build();
-		messageParticle.getDestinations().add(EUID.ONE);
+		MessageParticle messageParticle = new MessageParticle(
+			api.getMyAddress(),
+			api.getMyAddress(),
+			new byte[1],
+			new MetadataMap(),
+			0, ImmutableSet.of(api.getMyAddress().getUID(), EUID.ONE));
 		UnsignedAtom unsignedAtom = new UnsignedAtom(new Atom(ParticleGroup.of(SpunParticle.up(messageParticle)), System.currentTimeMillis()));
 		Atom signedAtom = this.identity.syncSign(unsignedAtom);
 		Result result = api.submitAtom(signedAtom);
