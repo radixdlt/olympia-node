f3347b0abd8dfd5b90281aaa5604a83cc626e4d4
Joshua Primero <josh@radixdlt.com>
2019-06-29 01:09:15 -0400

    Add method to get HTTP endpoint for a node

--- radixdlt-java/src/main/java/com/radixdlt/client/core/network/RadixNode.java
+++ radixdlt-java/src/main/java/com/radixdlt/client/core/network/RadixNode.java
@@ -11,6 +11,7 @@ public final class RadixNode {
 	private final int port;
 	private final Request webSocketEndpoint;
 	private final String webSocketUrl;
+	private final String httpUrl;
 
 	public RadixNode(String location, boolean useSSL, int port) {
 		this.location = location;
@@ -18,6 +19,7 @@ public final class RadixNode {
 		this.port = port;
 		this.webSocketUrl = (useSSL ? "wss://" : "ws://") + location + ":" + port + "/rpc";
 		this.webSocketEndpoint = new Request.Builder().url(webSocketUrl).build();
+		this.httpUrl = (useSSL ? "https://" : "http://") + location + ":" + port;
 	}
 
 	/**
@@ -26,6 +28,14 @@ public final class RadixNode {
 	 */
 	public Request getWebSocketEndpoint() {
 		return webSocketEndpoint;
+	}
+
+	/**
+	 * Get the http URL for this radix node
+	 * @return request object pointing to the http endpoint
+	 */
+	public Request getHttpEndpoint(String path) {
+		return new Request.Builder().url(this.httpUrl + path).build();
 	}
 
 	public int getPort() {
