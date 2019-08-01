17dfec09050a48e18490ffcbc4576d949efb453e
Joshua Primero <josh@radixdlt.com>
2019-08-01 14:27:32 +0100

    Use local radix engine package

--- radixdlt/src/main/java/com/radixdlt/serialization/mapper/MapperConstants.java
+++
@@ -1,9 +0,0 @@
-package com.radixdlt.serialization.mapper;
-
-final class MapperConstants {
-	private MapperConstants() {
-		throw new IllegalStateException("Can't construct");
-	}
-
-	static final String DSON_FILTER_NAME = "DSON";
-}
