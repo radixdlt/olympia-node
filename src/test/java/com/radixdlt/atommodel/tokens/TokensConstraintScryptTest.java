5560b592ec4a1e02bab6c56964602f5d36ae1794
Joshua Primero <josh@radixdlt.com>
2019-08-06 11:49:37 -0400

    Combine Token Constraints Scrypt

--- src/test/java/com/radixdlt/atommodel/tokens/TokensConstraintScryptTest.java
+++ src/test/java/com/radixdlt/atommodel/tokens/TokensConstraintScryptTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import com.google.common.collect.ImmutableMap;
 import com.radixdlt.atommodel.tokens.TokenDefinitionParticle.TokenTransition;
 import com.radixdlt.atomos.RRI;
 import com.radixdlt.atomos.RadixAddress;
@@ -11,17 +12,149 @@ import com.radixdlt.atomos.test.TestAtomOS;
 import com.radixdlt.atomos.test.TestResult;
 import com.radixdlt.constraintmachine.AtomMetadata;
 import com.radixdlt.utils.UInt256;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 
-public class TokenInstancesConstraintScryptTest {
+public class TokensConstraintScryptTest {
 	private static TestAtomOS testAtomOS = new TestAtomOS();
 
 	@BeforeClass
 	public static void initializeConstraintScrypt() {
-		TokenInstancesConstraintScrypt tokenInstancesConstraintScrypt = new TokenInstancesConstraintScrypt();
-		tokenInstancesConstraintScrypt.main(testAtomOS);
+		TokensConstraintScrypt tokensConstraintScrypt = new TokensConstraintScrypt();
+		tokensConstraintScrypt.main(testAtomOS);
+	}
+
+	@Test
+	public void when_validating_token_class_particle_without_symbol__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Symbol: no symbol provided");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_too_long_symbol__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getSymbol()).thenReturn("TEEEEEEEEEEEEEEEEEEEEEEEEEEST");
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Symbol: invalid length");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_too_short_symbol__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getSymbol()).thenReturn("");
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Symbol: invalid length");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_valid_symbol__result_has_no_symbol_errors() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getSymbol()).thenReturn("TEST");
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertNoErrorWithMessageContaining("Symbol:");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_without_description__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Description: no or empty provided");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_too_long_description__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getDescription()).thenReturn(
+			IntStream.range(0, TokenDefinitionParticle.MAX_DESCRIPTION_LENGTH + 1).mapToObj(i -> "c").collect(Collectors.joining()));
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Description: invalid length");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_valid_description__result_has_no_description_errors() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getDescription()).thenReturn("Valid test description.");
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertNoErrorWithMessageContaining("Description:");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_all_permissions_set__result_has_no_permission_errors() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Map<TokenTransition, TokenPermission> permissions = ImmutableMap.of(
+			TokenTransition.MINT, TokenPermission.TOKEN_CREATION_ONLY,
+			TokenTransition.BURN, TokenPermission.TOKEN_CREATION_ONLY
+		);
+
+		Mockito.when(token.getTokenPermissions()).thenReturn(permissions);
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertNoErrorWithMessageContaining("Permissions:");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_without_permissions_set__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getTokenPermissions()).thenReturn(new HashMap<>());
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Permissions: must be set");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_invalid_icon_url__result_has_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getIconUrl()).thenReturn("this is not a url");
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertErrorWithMessageContaining("Icon: not a valid URL");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_valid_icon__result_has_no_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		Mockito.when(token.getIconUrl()).thenReturn("https://assets.radixdlt.com/test.png");
+		testAtomOS
+			.testInitialParticle(token, PowerMockito.mock(AtomMetadata.class))
+			.assertNoErrorWithMessageContaining("Icon:");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_without_owner_signature__result_has_no_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		RadixAddress owner = PowerMockito.mock(RadixAddress.class);
+		Mockito.when(token.getOwner()).thenReturn(owner);
+		AtomMetadata metadata = PowerMockito.mock(AtomMetadata.class);
+		Mockito.when(metadata.isSignedBy(owner)).thenReturn(false);
+		testAtomOS
+			.testInitialParticle(token, metadata)
+			.assertErrorWithMessageContaining("sign");
+	}
+
+	@Test
+	public void when_validating_token_class_particle_with_owner_signature__result_has_no_error() {
+		TokenDefinitionParticle token = PowerMockito.mock(TokenDefinitionParticle.class);
+		RadixAddress owner = PowerMockito.mock(RadixAddress.class);
+		Mockito.when(token.getOwner()).thenReturn(owner);
+		AtomMetadata metadata = PowerMockito.mock(AtomMetadata.class);
+		Mockito.when(metadata.isSignedBy(owner)).thenReturn(true);
+		testAtomOS
+			.testInitialParticle(token, metadata)
+			.assertNoErrorWithMessageContaining("sign");
 	}
 
 	@Test
