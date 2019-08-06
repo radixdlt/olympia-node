b8b4f72e44527eca3746131060ece87b86777bdf
Joshua Primero <josh@radixdlt.com>
2019-08-06 11:51:54 -0400

    Remove unused methods

--- src/main/java/com/radixdlt/atommodel/tokens/Tokens.java
+++ src/main/java/com/radixdlt/atommodel/tokens/Tokens.java
@@ -1,10 +1,5 @@
 package com.radixdlt.atommodel.tokens;
 
-import com.radixdlt.common.EUID;
-import com.radixdlt.crypto.Hash;
-import com.radixdlt.utils.RadixConstants;
-import java.util.Objects;
-
 public final class Tokens {
 
 	private Tokens() {
@@ -18,25 +13,5 @@ public final class Tokens {
 	 */
 	public static String getNativeTokenShortCode() {
 		return "XRD";
-	}
-
-	/**
-	 * Returns the ID of the native asset of the network this node is a part of.
-	 *
-	 * @return The ID of the native asset.
-	 */
-	public static EUID getNativeTokenEUID() {
-		return shortCodeToEUID(getNativeTokenShortCode());
-	}
-
-	/**
-	 * Convert an asset short code ("ISO" code) into an ID.
-	 *
-	 * @param isoCode The asset short code to convert.
-	 * @return The ID of the asset short code.
-	 */
-	public static EUID shortCodeToEUID(String isoCode) {
-		Objects.requireNonNull(isoCode);
-		return new EUID(Hash.hash256(isoCode.getBytes(RadixConstants.STANDARD_CHARSET)));
 	}
 }
