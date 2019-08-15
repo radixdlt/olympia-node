1b284bf1bf097a4f98a4a167860ab596bf858ca3
Martin Sandiford <martin@radixdlt.com>
2019-08-15 10:40:01 +0100

    Made RadixTest a bit less chatty in the happy case

--- radixdlt/src/test/java/org/radix/serialization/RadixTest.java
+++ radixdlt/src/test/java/org/radix/serialization/RadixTest.java
@@ -42,7 +42,6 @@ public abstract class RadixTest
 	{
 		Field isRestricted = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
 
-		System.out.print("Encryption restrictions are set, need to override...");
 
 		if ( Modifier.isFinal(isRestricted.getModifiers()) )
 		{
@@ -53,9 +52,12 @@ public abstract class RadixTest
 
 		try {
 			isRestricted.setAccessible(true);
-			isRestricted.setBoolean(null, false);
+			if (isRestricted.get(null).equals(Boolean.TRUE)) {
+				System.out.print("Encryption restrictions are set, need to override...");
+				isRestricted.setBoolean(null, false);
+				System.out.println("...override success!");
+			}
 			isRestricted.setAccessible(false);
-			System.out.println("...override success!");
 		} catch (IllegalAccessException e) {
 			System.out.println("...failed.  Best of luck.");
 		}
