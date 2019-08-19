f1209dfd0d183864ba8359543021b40bd419702c
Florian Cäsar <florian.caesar@outlook.com>
2019-08-19 13:51:22 +0200

    Remove old Unique and DuplicateIndexCreator

--- radixdlt/src/main/java/com/radixdlt/ledger/UniqueIndexCreator.java
+++
@@ -1,8 +0,0 @@
-package com.radixdlt.ledger;
-
-import java.util.Set;
-import org.radix.atoms.Atom;
-
-public interface UniqueIndexCreator {
-	void createUniqueIndexables(Atom atom, Set<LedgerIndex> uniqueIndexables);
-}
