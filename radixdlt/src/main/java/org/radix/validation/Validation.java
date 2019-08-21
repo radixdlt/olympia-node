e3ef510d00e04954779f47c82ca7c47e62cab6db
Joshua Primero <josh@radixdlt.com>
2019-08-21 21:36:50 +0800

    Update AtomDriver constructor

--- radixdlt/src/main/java/org/radix/validation/Validation.java
+++ radixdlt/src/main/java/org/radix/validation/Validation.java
@@ -17,7 +17,6 @@ import org.radix.modules.Modules;
 import org.radix.modules.Plugin;
 import org.radix.modules.exceptions.ModuleException;
 import org.radix.properties.RuntimeProperties;
-import com.radixdlt.serialization.Serialization;
 import org.radix.time.Time;
 import com.radixdlt.universe.Universe;
 import org.radix.universe.system.LocalSystem;
@@ -37,7 +36,6 @@ public class Validation extends Plugin {
 		final AtomDriver atomDriver = new AtomDriver(
 			() -> Modules.get(Universe.class),
 			Time::currentTimestamp,
-			Modules.get(Serialization.class),
 			skipAtomFeeCheck,
 			Time.MAXIMUM_DRIFT
 		);
