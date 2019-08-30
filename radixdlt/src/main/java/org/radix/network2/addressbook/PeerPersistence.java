e4f81f8dd22c3e0ed3ba45426cdbe4987812e346
Martin Sandiford <martin@radixdlt.com>
2019-08-30 15:08:02 +0100

    Update documentation for persistence interface

--- radixdlt/src/main/java/org/radix/network2/addressbook/PeerPersistence.java
+++ radixdlt/src/main/java/org/radix/network2/addressbook/PeerPersistence.java
@@ -4,8 +4,31 @@ import java.util.function.Consumer;
 
 import com.radixdlt.common.EUID;
 
+/**
+ * Persistence interface for persisting peers in AddressBook.
+ */
 public interface PeerPersistence {
+
+	/**
+	 * Saves peer to database.
+	 *
+	 * @param peer the peer to save.  The peer must have a valid NID.
+	 * @return {@code true} if the peer was saved, {@code false} otherwise
+	 */
 	boolean savePeer(Peer peer);
+
+	/**
+	 * Deletes peer identified by specified NID.
+	 *
+	 * @param NID of the peer to delete
+	 * @return {@code true} if the peer was deleted, {@code false} otherwise
+	 */
 	boolean deletePeer(EUID nid);
+
+	/**
+	 * Calls the specified consumer with each persisted peer.
+	 *
+	 * @param c the consumer to call with each persisted peer
+	 */
 	void forEachPersistedPeer(Consumer<Peer> c);
 }
