7ba035c5e4d3cbb77fc740e426c685432650e94f
Martin Sandiford <martin@radixdlt.com>
2019-11-11 12:58:18 +0000

    Added logic to prevent information leakage

--- src/main/java/com/radixdlt/crypto/ECKeyUtils.java
+++ src/main/java/com/radixdlt/crypto/ECKeyUtils.java
@@ -114,27 +114,60 @@ class ECKeyUtils {
 		}
 	}
 
+	/**
+	 * Adjusts the specified array so that is is equal to the specified length.
+	 * <ul>
+	 *   <li>
+	 *     If the array is equal to the specified length, it is returned
+	 *     without change.
+	 *   </li>
+	 *   <li>
+	 *     If array is shorter than the specified length, a new array that
+	 *     is zero padded at the front is returned.  The specified array is
+	 *     filled with zeros to prevent information leakage.
+	 *   </li>
+	 *   <li>
+	 *     If the array is longer than the specified length, a new array
+	 *     with sufficient leading zeros removed is returned.  The specified
+	 *     array is filled with zeros to prevent information leakage.
+	 *     An {@code IllegalArgumentException} is thrown if the specified
+	 *     array does not have sufficient leading zeros to allow it to be
+	 *     truncated to the specified length.
+	 *   </li>
+	 * </ul>
+	 * @param array The specified array
+	 * @param length The specified length
+	 * @return An array of the specified length as described above
+	 * @throws IllegalArgumentException if the specified array is longer than
+	 * 		the specified length, and does not have sufficient leading zeros
+	 * 		to allow truncation to the specified length.
+	 * @throws NullPointerException if the specified array is {@code null}
+	 */
 	static byte[] adjustArray(byte[] array, int length) {
 		if (length == array.length) {
 			// Length is fine
 			return array;
 		}
+		final byte[] result;
 		if (length > array.length) {
 			// Needs zero padding at front
-			byte[] result = new byte[length];
+			result = new byte[length];
 			System.arraycopy(array, 0, result, length - array.length, array.length);
-			return result;
-		}
-		// Need to drop zeros at front -> error if dropped bytes are not zero
-		int offset = 0;
-		while (array.length - offset > length) {
-			if (array[offset] != 0) {
-				throw new IllegalArgumentException(String.format("Array is greater than %s bytes: %s", length, Bytes.toHexString(array)));
+		} else {
+			// Must be longer, need to drop zeros at front -> error if dropped bytes are not zero
+			int offset = 0;
+			while (array.length - offset > length) {
+				if (array[offset] != 0) {
+					throw new IllegalArgumentException(String.format("Array is greater than %s bytes: %s", length, Bytes.toHexString(array)));
+				}
+				offset += 1;
 			}
-			offset += 1;
+			// Now copy length bytes from offset within array
+			result = Arrays.copyOfRange(array, offset, offset + length);
 		}
-		// Now return length bytes from offset within array
-		return Arrays.copyOfRange(array, offset, offset + length);
+		// Zero out original array so as to avoid information leaks
+		Arrays.fill(array, (byte) 0);
+		return result;
 	}
 
 	@VisibleForTesting
