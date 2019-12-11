a33c74b664e3a093dfa8006ff07f358f76923354
Florian Cäsar <florian@radixdlt.com>
2019-12-11 11:05:37 +0100

    Make EventListener non-comparable

--- radixdlt/src/main/java/org/radix/events/EventListener.java
+++ radixdlt/src/main/java/org/radix/events/EventListener.java
@@ -3,29 +3,12 @@ package org.radix.events;
 import org.radix.common.Syncronicity;
 import org.radix.events.Event.EventPriority;
 
-public interface EventListener<T extends Event> extends Comparable<EventListener<?>>
+public interface EventListener<T extends Event>
 {
 	public void process(T event) throws Throwable;
-
-	public default int getPriority()
-	{
-		return EventPriority.DEFAULT.priority();
-	}
 
 	public default Syncronicity getSyncronicity()
 	{
 		return Syncronicity.ASYNCRONOUS;
-	}
-
-	@Override
-	default int compareTo(EventListener<?> other)
-	{
-		if (this.getPriority() > other.getPriority())
-			return -1;
-
-		if (this.getPriority() < other.getPriority())
-			return 1;
-
-		return 0;
 	}
 }
