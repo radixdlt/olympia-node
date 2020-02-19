162d683eee90042676308195597221c722b7cd35
Florian Cäsar <florian@radixdlt.com>
2020-02-19 10:55:05 +0100

    Add equals/HC/toString to Timeout

--- radixdlt/src/main/java/com/radixdlt/consensus/Timeout.java
+++ radixdlt/src/main/java/com/radixdlt/consensus/Timeout.java
@@ -1,5 +1,7 @@
 package com.radixdlt.consensus;
 
+import java.util.Objects;
+
 /**
  * Represents a timeout in the pacemaker
  */
@@ -12,5 +14,29 @@ public final class Timeout {
 
 	public long getRound() {
 		return round;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Timeout timeout = (Timeout) o;
+		return round == timeout.round;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(round);
+	}
+
+	@Override
+	public String toString() {
+		return "Timeout{" +
+			"round=" + round +
+			'}';
 	}
 }
