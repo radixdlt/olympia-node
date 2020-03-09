ad9eac07c08869f7241e0216d8dbf09354a2e95d
Martin Sandiford <martin@radixdlt.com>
2020-03-09 09:16:54 +0000

    Fix checkstyle warnings

--- radixdlt/src/test/java/com/radixdlt/store/StoreIndexSerializeTest.java
+++ radixdlt/src/test/java/com/radixdlt/store/StoreIndexSerializeTest.java
@@ -1,7 +1,5 @@
 package com.radixdlt.store;
 
-import com.radixdlt.common.AID;
-import com.radixdlt.crypto.Hash;
 import com.radixdlt.utils.Bytes;
 import org.radix.serialization.SerializeMessageObject;
 
--- radixdlt/src/test/java/com/radixdlt/store/StoreIndexTest.java
+++ radixdlt/src/test/java/com/radixdlt/store/StoreIndexTest.java
@@ -1,7 +1,6 @@
 package com.radixdlt.store;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
-import nl.jqno.equalsverifier.Warning;
 import org.junit.Test;
 
 public class StoreIndexTest {
