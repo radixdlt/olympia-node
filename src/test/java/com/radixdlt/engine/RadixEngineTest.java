1a4b29a54a3b98a3beace7ec8caa1ca0599a2867
Joshua Primero <josh@radixdlt.com>
2020-03-13 22:08:53 +0800

    Fix checkstyle issues

--- src/test/java/com/radixdlt/engine/RadixEngineTest.java
+++ src/test/java/com/radixdlt/engine/RadixEngineTest.java
@@ -34,10 +34,8 @@ import org.junit.Test;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class RadixEngineTest {
