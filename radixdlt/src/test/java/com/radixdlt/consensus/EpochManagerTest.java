bd8d37f0c41d6e54dc6052f34c4a7f6c7e6b8051
Joshua Primero <josh@radixdlt.com>
2020-03-25 18:10:48 +0800

    Add unit tests

---
+++ radixdlt/src/test/java/com/radixdlt/consensus/EpochManagerTest.java
@@ -0,0 +1,35 @@
+package com.radixdlt.consensus;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import com.radixdlt.consensus.liveness.Pacemaker;
+import com.radixdlt.consensus.liveness.ProposalGenerator;
+import com.radixdlt.consensus.safety.SafetyRules;
+import com.radixdlt.consensus.validators.Validator;
+import com.radixdlt.consensus.validators.ValidatorSet;
+import com.radixdlt.crypto.ECKeyPair;
+import com.radixdlt.mempool.Mempool;
+import java.util.Collections;
+import org.junit.Test;
+
+public class EpochManagerTest {
+	@Test
+	public void when_next_epoch__then_should_create_new_event_coordinator() throws Exception {
+		EpochManager epochManager = new EpochManager(
+			mock(ProposalGenerator.class),
+			mock(Mempool.class),
+			mock(EventCoordinatorNetworkSender.class),
+			mock(SafetyRules.class),
+			mock(Pacemaker.class),
+			mock(VertexStore.class),
+			mock(PendingVotes.class),
+			mock(ECKeyPair.class)
+		);
+
+		ECKeyPair ecKeyPair = new ECKeyPair();
+		Validator validator = Validator.from(ecKeyPair.getPublicKey());
+		EventCoordinator eventCoordinator = epochManager.nextEpoch(ValidatorSet.from(Collections.singleton(validator)));
+		assertNotNull(eventCoordinator);
+	}
+}
