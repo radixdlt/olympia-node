c5473248c15062f5a42b363acc9438e035c87342
Alexander Cyon <alex.cyon@gmail.com>
2020-03-25 14:45:52 +0100

    fixed java doc

--- src/main/java/com/radixdlt/crypto/SignatureScheme.java
+++ src/main/java/com/radixdlt/crypto/SignatureScheme.java
@@ -21,8 +21,7 @@ public enum SignatureScheme {
 
     /**
      *  Elliptic Curve Digital Signature Algorithm, or ECDSA for short. A good introduction is to be found
-     *  <a href="https://en.wikipedia.org/wiki/
-     *  * Elliptic_Curve_Digital_Signature_Algorithm">here on wikipedia</a>.
+     *  <a href="https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm">here on wikipedia</a>.
      */
     ECDSA;
 
