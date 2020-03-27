b908b930394775b325afa07767d40e9ef504db6e
shambupujar <shambu@radixdlt.com>
2020-03-27 14:17:18 +0000

    GetMessage completed

--- radixdlt-cli/src/main/groovy/com/radixdlt/cli/GetMessage.groovy
+++ radixdlt-cli/src/main/groovy/com/radixdlt/cli/GetMessage.groovy
@@ -1,9 +1,7 @@
 package com.radixdlt.cli
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+
 import com.radixdlt.client.application.RadixApplicationAPI
-import com.radixdlt.client.atommodel.accounts.RadixAddress
 import com.radixdlt.client.core.Bootstrap
 import picocli.CommandLine
 
@@ -16,21 +14,9 @@ class GetMessage implements Runnable {
 
     void run() {
 
-        RadixApplicationAPI api = RadixApplicationAPI.create(Bootstrap.SUNSTONE, Utils.getIdentity(identityInfo))
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(RadixAddress.class, new RadixAddressTypeAdapter())
-                .excludeFieldsWithoutExposeAnnotation()
-                .create()
-
-//        TODO Below api doesn't have getMessages
-//        api.pull();
-//        api.getMessages()
-//                .map(gson::toJson)
-//                .subscribe(a -> {
-//                    System.out.println(a);
-//                    System.out.flush();
-//                });
-//        TimeUnit.SECONDS.sleep(3);
+        RadixApplicationAPI api = RadixApplicationAPI.create(Bootstrap.LOCALHOST_SINGLENODE, Utils.getIdentity(identityInfo))
+        api.pull()
+        api.observeMessages().subscribe({ it -> println it })
     }
 
 }
