a6b5e58354b928802e5f2c07fd2467fb142a42fd
Joshua Primero <josh@radixdlt.com>
2020-04-01 15:14:06 +0800

    Cleanup VoteData

--- radixdlt/src/main/java/com/radixdlt/consensus/VoteData.java
+++ radixdlt/src/main/java/com/radixdlt/consensus/VoteData.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2020 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
 package com.radixdlt.consensus;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,6 +27,12 @@ import java.util.Objects;
 import java.util.Optional;
 import javax.annotation.concurrent.Immutable;
 
+/**
+ * Serves as data which is voted upon in a round of BFT. In a standard implementation this would
+ * only include a proposed vertex which extends the chain. In this implementation we include data
+ * for both the parent and any vertex which would be committed if this data reaches a quorum of votes.
+ * This way, vote processing can be handled statelessly.
+ */
 @Immutable
 @SerializerId2("consensus.vote_data")
 public final class VoteData {
@@ -68,11 +91,12 @@ public final class VoteData {
 		}
 		VoteData that = (VoteData) o;
 		return Objects.equals(proposed, that.proposed)
-			&& Objects.equals(parent, that.parent);
+			&& Objects.equals(parent, that.parent)
+			&& Objects.equals(committed, that.committed);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(proposed, parent);
+		return Objects.hash(proposed, parent, committed);
 	}
 }
