db9e215755e64c1a34e9d36eb375c8083b822184
Florian Cäsar <florian@radixdlt.com>
2020-04-10 12:21:30 +0200

    Add LatentDockerNetworkTest skeleton

---
+++ system-tests/src/test/java/com/radixdlt/test/LatentDockerNetworkTest.java
@@ -0,0 +1,51 @@
+package com.radixdlt.test;
+
+import io.reactivex.Observable;
+import org.junit.Test;
+import utils.CmdHelper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LatentDockerNetworkTest {
+
+	// copied from ValidatorSet in core
+	static int acceptableFaults(int n) {
+		// Compute acceptable faults based on Byzantine limit n = 3f + 1
+		// i.e. f = (n - 1) / 3
+		return (n - 1) / 3;
+	}
+
+	@Test
+	public void given_4_correct_bfts_in_latent_network__then_all_instances_should_get_same_commits_consecutive_vertices_eventually_over_1_minute() {
+		final int numNodes = 4;
+		final int quorumSize = numNodes - acceptableFaults(numNodes);
+		final long runtimeSeconds = 1;
+		final long refreshIntervalMillis = 500;
+		final TimeUnit timeUnit = TimeUnit.MINUTES;
+
+		Map<String, Map<String, Object>> dockerOptionsPerNode = CmdHelper.getDockerOptions(numNodes, quorumSize);
+		CmdHelper.removeAllDockerContainers();
+		CmdHelper.runCommand("docker network rm ${networkName}", null, true);
+		CmdHelper.runCommand("docker network create ${networkName}");
+
+		dockerOptionsPerNode.forEach((nodeName, options) -> {
+			List<Object> dockerSetup = CmdHelper.node(options);
+			String[] dockerEnv = (String[]) dockerSetup.get(0);
+			String dockerCommand = (String) dockerSetup.get(1);
+			CmdHelper.runCommand(dockerCommand, dockerEnv);
+		});
+
+		CmdHelper.checkNGenerateKey();
+
+
+		// TODO fetch consensus data from each node and check every refreshInterval until runtimeSeconds is over
+		// see LatentNetwork integration test in core
+//		dockerOptionsPerNode.keySet().stream()
+//			.map(nodeName -> Observable.interval(refreshIntervalMillis, TimeUnit.MILLISECONDS)
+//				.map(i -> ))
+
+
+	}
+}
