a34b35e86c9a514e7418589d00fd0db07db794f0
Martin Sandiford <martin@radixdlt.com>
2020-04-21 11:36:21 +0100

    Speed up BootstrapDiscoveryTest

--- radixdlt/src/test/java/org/radix/network/discovery/BootstrapDiscoveryTest.java
+++ radixdlt/src/test/java/org/radix/network/discovery/BootstrapDiscoveryTest.java
@@ -19,41 +19,31 @@ package org.radix.network.discovery;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import com.google.common.collect.Iterables;
+import com.radixdlt.network.addressbook.AddressBook;
+import com.radixdlt.network.addressbook.PeerPredicate;
+import com.radixdlt.network.addressbook.PeerWithTransport;
+import com.radixdlt.network.transport.TransportInfo;
+import com.radixdlt.network.transport.tcp.TCPConstants;
 import com.radixdlt.properties.RuntimeProperties;
 import com.radixdlt.universe.Universe;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.powermock.api.mockito.PowerMockito.doAnswer;
-import static org.powermock.api.mockito.PowerMockito.doReturn;
-import static org.powermock.api.mockito.PowerMockito.doThrow;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.spy;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore({"java.security.*", "javax.security.*", "org.bouncycastle.*", "javax.management.*"})
-@PrepareForTest({BootstrapDiscovery.class, URL.class, URLConnection.class, RuntimeProperties.class, Universe.class, Thread.class})
 public class BootstrapDiscoveryTest {
     // Mocks
     private RuntimeProperties config;
@@ -61,37 +51,16 @@ public class BootstrapDiscoveryTest {
     private URLConnection conn;
     private Universe universe;
 
-    // Methods
-    private Method toHost;
-    private Method getNextNode;
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Before
     public void setUp() throws IOException {
         // create stubs
-        spy(BootstrapDiscovery.class);
-        spy(Thread.class);
         config = defaultProperties();
         universe = mock(Universe.class);
         url = mock(URL.class);
         conn = mock(URLConnection.class);
 
-        // Expose private methods
-        toHost = Whitebox.getMethod(BootstrapDiscovery.class, "toHost", new byte[0].getClass(), int.class);
-        getNextNode = Whitebox.getMethod(BootstrapDiscovery.class, "getNextNode", URL.class);
-
         // initialize stubs
-        doReturn(30).when(config).get("network.discovery.connection.retries", 30);
-        doReturn(1).when(config).get("network.discovery.connection.cooldown", 1);
-        doReturn(60000).when(config).get("network.discovery.connection.timeout", 60000);
-        doReturn(60000).when(config).get("network.discovery.read.timeout", 60000);
-        doReturn(0).when(config).get("network.discovery.allow_tls_bypass", 0);
-        doReturn(8).when(config).get("network.connections.in", 8);
-        doReturn(8).when(config).get("network.connections.out", 8);
         doReturn(8192).when(config).get("messaging.inbound.queue_max", 8192);
-        doReturn(1 << 18).when(config).get("network.udp.buffer", 1 << 18);
 
         when(config.get(eq("network.discovery.connection.retries"), anyInt())).thenReturn(1);
         when(config.get(eq("network.discovery.connection.cooldown"), anyInt())).thenReturn(1);
@@ -114,71 +83,90 @@ public class BootstrapDiscoveryTest {
     }
 
 	@Test
-    public void testToHost_Empty() throws ReflectiveOperationException
-    {
-        assertEquals("",  toHost.invoke(null, new byte[0], 0));
+    public void testToHost_Empty() {
+        assertEquals("",  BootstrapDiscovery.toHost(new byte[0], 0));
     }
 
 	@Test
-    public void testToHost() throws ReflectiveOperationException
-    {
+    public void testToHost() {
         for (int b = Byte.MIN_VALUE; b <= Byte.MAX_VALUE; b++) {
             byte[] buf = new byte[] {(byte) b};
-            if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.".indexOf(0xff & b) != -1) {
-                assertEquals(new String(buf, StandardCharsets.US_ASCII), toHost.invoke(null, buf, buf.length));
+            if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:-.".indexOf(0xff & b) != -1) {
+                assertEquals(new String(buf, StandardCharsets.US_ASCII), BootstrapDiscovery.toHost(buf, buf.length));
             } else {
-                assertEquals(null,  toHost.invoke(null, buf, buf.length));
+                assertNull(BootstrapDiscovery.toHost(buf, buf.length));
             }
         }
     }
 
     @Test
-	public void testGetNextNode() throws IOException, ReflectiveOperationException
-	{
+	public void testGetNextNode() throws IOException {
         String expected = "1.1.1.1";
         doReturn(expected.length()).when(conn).getContentLength();
         doReturn(new ByteArrayInputStream(expected.getBytes(StandardCharsets.US_ASCII))).when(conn).getInputStream();
-        assertEquals(expected,  getNextNode.invoke(new BootstrapDiscovery(config, universe), url));
+        assertEquals(expected, new BootstrapDiscovery(config, universe).getNextNode(url));
     }
 
     @Test
-	public void testGetNextNode_RuntimeException() throws IOException, ReflectiveOperationException
-	{
+	public void testGetNextNode_RuntimeException() throws IOException {
         doThrow(new RuntimeException()).when(conn).connect();
-        assertEquals(null,  getNextNode.invoke(new BootstrapDiscovery(config, universe), url));
+        assertNull(new BootstrapDiscovery(config, universe).getNextNode(url));
     }
 
-    @Test
-	public void testGetNextNode_InterruptedException() throws ReflectiveOperationException, InterruptedException
-	{
+	public void testGetNextNode_InterruptedException() throws InterruptedException {
         doThrow(new InterruptedException()).when(Thread.class);
         Thread.sleep(anyLong());
-        assertEquals(null,  getNextNode.invoke(new BootstrapDiscovery(config, universe), url));
+        assertNull(new BootstrapDiscovery(config, universe).getNextNode(url));
     }
 
     @Test
-	public void testConstructor_Seeds() throws Exception
-	{
+	public void testConstructor_Seeds() {
         doReturn("").when(config).get("network.discovery.urls", "");
         doReturn("1.1.1.1").when(config).get("network.seeds", "");
-        doReturn(8).when(config).get(eq("network.connections.in"), anyInt());
-        doReturn(8).when(config).get(eq("network.connections.out"), anyInt());
-        doReturn(8000).when(config).get(eq("messaging.inbound.queue_max"), anyInt());
-        doReturn(8000).when(config).get(eq("messaging.outbound.queue_max"), anyInt());
-        doReturn(30).when(config).get(eq("messaging.time_to_live"), anyInt());
-        BootstrapDiscovery testSubject = Whitebox.invokeConstructor(BootstrapDiscovery.class, config, universe);
+        BootstrapDiscovery testSubject = new BootstrapDiscovery(config, universe);
         Set<?> hosts = Whitebox.getInternalState(testSubject, "hosts");
         assertEquals(1, hosts.size());
     }
 
-    @Test
-	public void testConstructor_NeedHTTPS() throws Exception
-	{
-        thrown.expect(IllegalStateException.class);
+    @Test(expected = IllegalStateException.class)
+	public void testConstructor_NeedHTTPS() {
         doReturn("http://example.com").when(config).get("network.discovery.urls", "");
-        Whitebox.invokeConstructor(BootstrapDiscovery.class, config, universe);
+        assertNotNull(new BootstrapDiscovery(config, universe));
     }
 
+    @Test
+    public void testDiscoverNoFilter() throws UnknownHostException {
+        doReturn("").when(config).get("network.discovery.urls", "");
+        doReturn("1.1.1.1").when(config).get("network.seeds", "");
+    	BootstrapDiscovery discovery = new BootstrapDiscovery(config, universe);
+
+    	TransportInfo ti = discovery.toDefaultTransportInfo("1.1.1.1");
+    	PeerWithTransport pwt = new PeerWithTransport(ti);
+    	AddressBook addressbook = mock(AddressBook.class);
+    	doReturn(pwt).when(addressbook).peer(ti);
+    	Collection<TransportInfo> results = discovery.discover(addressbook, null);
+    	assertEquals(ti, Iterables.getOnlyElement(results));
+    }
+
+    @Test
+    public void testDiscoverFilter() throws UnknownHostException {
+        doReturn("").when(config).get("network.discovery.urls", "");
+        doReturn("1.1.1.1,2.2.2.2,3.3.3.3").when(config).get("network.seeds", "");
+    	BootstrapDiscovery discovery = new BootstrapDiscovery(config, universe);
+
+    	TransportInfo ti1 = discovery.toDefaultTransportInfo("1.1.1.1");
+    	PeerWithTransport pwt1 = new PeerWithTransport(ti1);
+    	TransportInfo ti2 = discovery.toDefaultTransportInfo("2.2.2.2");
+    	PeerWithTransport pwt2 = new PeerWithTransport(ti2);
+    	TransportInfo ti3 = discovery.toDefaultTransportInfo("3.3.3.3");
+    	AddressBook addressbook = mock(AddressBook.class);
+    	doReturn(pwt1).when(addressbook).peer(ti1);
+    	doReturn(pwt2).when(addressbook).peer(ti2);
+    	doReturn(null).when(addressbook).peer(ti3);
+    	PeerPredicate predicate = p -> p.connectionData("TCP").get(TCPConstants.METADATA_HOST).equals("1.1.1.1");
+    	Collection<TransportInfo> results = discovery.discover(addressbook, predicate);
+    	assertEquals(ti1, Iterables.getOnlyElement(results));
+    }
     private static RuntimeProperties defaultProperties() {
         RuntimeProperties properties = mock(RuntimeProperties.class);
         doAnswer(invocation -> invocation.getArgument(1)).when(properties).get(any(), any());
