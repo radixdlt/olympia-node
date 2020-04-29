63d1dec1c743fad95c88074bc1695245c2b9fad7
Joshua Primero <josh@radixdlt.com>
2020-04-29 18:44:48 +0800

    Add lcm support for UInt256

--- src/test/java/com/radixdlt/utils/MathUtilsTest.java
+++ src/test/java/com/radixdlt/utils/MathUtilsTest.java
@@ -5,23 +5,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.LongFunction;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 import org.junit.Test;
 
 public class MathUtilsTest {
-
-	private static final BigInteger UINT128_MAX_BI = new BigInteger(1, UInt128.MAX_VALUE.toByteArray());
-
-	@Test
-	public void testOneAndMax() {
-		assertThat(MathUtils.cappedLCM(UInt128.MAX_VALUE, UInt128.ONE, UInt128.MAX_VALUE))
-			.isEqualTo(UInt128.MAX_VALUE);
-	}
-
-	@Test
-	public void testPrimeLCM() {
+	private static <T> void testPrimeLCM(
+		IntFunction<T[]> arrayBuilder,
+		LongFunction<T> builder,
+		BinaryOperator<T> multiply,
+		T one,
+		T max,
+		Function<T, BigInteger> toBI,
+		BiFunction<T, T[], T> lcmFunction
+	) {
 		List<Integer> primeList = Arrays.asList(
 			2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
 			31, 37, 41, 43, 47, 53, 59, 61, 67,
@@ -32,49 +34,123 @@ public class MathUtilsTest {
 			.mapToLong(i -> i)
 			.limit(size);
 
+		BigInteger maxBI = toBI.apply(max);
 		for (int size = 1; size < primeList.size(); size++) {
-			UInt128[] values = primeNumbers.apply(size)
-				.mapToObj(UInt128::from)
-				.toArray(UInt128[]::new);
+			T[] values = primeNumbers.apply(size)
+				.mapToObj(builder)
+				.toArray(arrayBuilder);
 
 			BigInteger biExpected = primeNumbers.apply(size)
 				.mapToObj(BigInteger::valueOf)
 				.reduce(BigInteger.ONE, BigInteger::multiply);
 
-			final UInt128 expected;
-			if (biExpected.compareTo(UINT128_MAX_BI) > 0) {
+			final T expected;
+			if (biExpected.compareTo(maxBI) > 0) {
 				expected = null;
 			} else {
 				expected = primeNumbers.apply(size)
-					.mapToObj(UInt128::from)
-					.reduce(UInt128.ONE, UInt128::multiply);
+					.mapToObj(builder)
+					.reduce(one, multiply);
 			}
 
-			assertThat(MathUtils.cappedLCM(UInt128.MAX_VALUE, values))
+			assertThat(lcmFunction.apply(max, values))
 				.isEqualTo(expected);
 		}
 	}
 
-	@Test
-	public void testGeometricLCM() {
+	private static <T> void testGeometricLCM(
+		IntFunction<T[]> arrayBuilder,
+		LongFunction<T> builder,
+		BinaryOperator<T> multiply,
+		BiFunction<T, Integer, T> pow,
+		T max,
+		Function<T, BigInteger> toBI,
+		BiFunction<T, T[], T> lcmFunction
+	) {
+
+		BigInteger maxBI = toBI.apply(max);
 		for (int exponent = 1; exponent < 12; exponent++) {
 			for (int base = 2; base < 1024; base++) {
-				final UInt128 base128 = UInt128.from(base);
-				UInt128[] values = Stream.iterate(base128, n -> n.multiply(base128))
+				final T baseNum = builder.apply(base);
+				final T[] values = Stream.iterate(baseNum, n -> multiply.apply(n, baseNum))
 					.limit(exponent)
-					.toArray(UInt128[]::new);
+					.toArray(arrayBuilder);
 
 				final BigInteger biExpected = BigInteger.valueOf(base).pow(exponent);
-				final UInt128 expected;
-				if (biExpected.compareTo(UINT128_MAX_BI) > 0) {
+				final T expected;
+				if (biExpected.compareTo(maxBI) > 0) {
 					expected = null;
 				} else {
-					expected = UInt128.from(base).pow(exponent);
+					expected = pow.apply(builder.apply(base), exponent);
 				}
 
-				assertThat(MathUtils.cappedLCM(UInt128.MAX_VALUE, values))
+				assertThat(lcmFunction.apply(max, values))
 					.isEqualTo(expected);
 			}
 		}
+	}
+
+	@Test
+	public void testOneAndMax128() {
+		assertThat(MathUtils.cappedLCM(UInt128.MAX_VALUE, UInt128.ONE, UInt128.MAX_VALUE))
+			.isEqualTo(UInt128.MAX_VALUE);
+	}
+
+	@Test
+	public void testPrimeLCM128() {
+		testPrimeLCM(
+			UInt128[]::new,
+			UInt128::from,
+			UInt128::multiply,
+			UInt128.ONE,
+			UInt128.MAX_VALUE,
+			i -> new BigInteger(1, i.toByteArray()),
+			MathUtils::cappedLCM
+		);
+	}
+
+	@Test
+	public void testGeometricLCM128() {
+		testGeometricLCM(
+			UInt128[]::new,
+			UInt128::from,
+			UInt128::multiply,
+			UInt128::pow,
+			UInt128.MAX_VALUE,
+			i -> new BigInteger(1, i.toByteArray()),
+			MathUtils::cappedLCM
+		);
+	}
+
+	@Test
+	public void testOneAndMax256() {
+		assertThat(MathUtils.cappedLCM(UInt256.MAX_VALUE, UInt256.ONE, UInt256.MAX_VALUE))
+			.isEqualTo(UInt256.MAX_VALUE);
+	}
+
+	@Test
+	public void testPrimeLCM256() {
+		testPrimeLCM(
+			UInt256[]::new,
+			UInt256::from,
+			UInt256::multiply,
+			UInt256.ONE,
+			UInt256.MAX_VALUE,
+			i -> new BigInteger(1, i.toByteArray()),
+			MathUtils::cappedLCM
+		);
+	}
+
+	@Test
+	public void testGeometricLCM256() {
+		testGeometricLCM(
+			UInt256[]::new,
+			UInt256::from,
+			UInt256::multiply,
+			UInt256::pow,
+			UInt256.MAX_VALUE,
+			i -> new BigInteger(1, i.toByteArray()),
+			MathUtils::cappedLCM
+		);
 	}
 }
