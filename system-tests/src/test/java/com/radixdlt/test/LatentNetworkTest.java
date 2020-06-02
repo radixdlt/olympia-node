10e53f8d272813fed05213d07c656e00abc45c8d
shambupujar <shambu@radixdlt.com>
2020-06-02 17:44:05 +0100

    More formatting

--- system-tests/src/test/java/com/radixdlt/test/LatentNetworkTest.java
+++ system-tests/src/test/java/com/radixdlt/test/LatentNetworkTest.java
@@ -16,8 +16,14 @@ public class LatentNetworkTest {
 	 * @return The test builder
 	 */
 	static RemoteBFTTest.Builder latentTestBuilder() {
-		return RemoteBFTTest.builder().assertResponsiveness().assertAllProposalsHaveDirectParents().assertNoRejectedProposals()
-			.assertNoSyncExceptions().assertNoTimeouts().assertSafety().assertLiveness();
+		return RemoteBFTTest.builder()
+			.assertResponsiveness()
+			.assertAllProposalsHaveDirectParents()
+			.assertNoRejectedProposals()
+			.assertNoSyncExceptions()
+			.assertNoTimeouts()
+			.assertSafety()
+			.assertLiveness();
 	}
 
 	@Test
@@ -25,7 +31,11 @@ public class LatentNetworkTest {
 	public void given_3_correct_bfts_in_latent_docker_network__then_all_instances_should_get_same_commits_and_progress_should_be_made() {
 		try (DockerNetwork network = DockerNetwork.builder().numNodes(3).build()) {
 			network.startBlocking();
-			RemoteBFTTest test = latentTestBuilder().network(RemoteBFTNetworkBridge.of(network)).waitUntilResponsive().startConsensusOnRun().build();
+			RemoteBFTTest test = latentTestBuilder()
+				.network(RemoteBFTNetworkBridge.of(network))
+				.waitUntilResponsive()
+				.startConsensusOnRun()
+				.build();
 			test.runBlocking(1, TimeUnit.MINUTES);
 		}
 	}
@@ -35,7 +45,11 @@ public class LatentNetworkTest {
 	public void given_4_correct_bfts_in_latent_docker_network__then_all_instances_should_get_same_commits_and_progress_should_be_made() {
 		try (DockerNetwork network = DockerNetwork.builder().numNodes(4).build()) {
 			network.startBlocking();
-			RemoteBFTTest test = latentTestBuilder().network(RemoteBFTNetworkBridge.of(network)).waitUntilResponsive().startConsensusOnRun().build();
+			RemoteBFTTest test = latentTestBuilder()
+				.network(RemoteBFTNetworkBridge.of(network))
+				.waitUntilResponsive()
+				.startConsensusOnRun()
+				.build();
 			test.runBlocking(1, TimeUnit.MINUTES);
 		}
 	}
@@ -44,7 +58,9 @@ public class LatentNetworkTest {
 	@Category(Cluster.class)
 	public void given_4_correct_bfts_in_latent_cluster_network__then_all_instances_should_get_same_commits_and_progress_should_be_made() {
 		final StaticClusterNetwork network = StaticClusterNetwork.extractFromProperty(4);
-		RemoteBFTTest test = latentTestBuilder().network(RemoteBFTNetworkBridge.of(network)).waitUntilResponsive()
+		RemoteBFTTest test = latentTestBuilder()
+			.network(RemoteBFTNetworkBridge.of(network))
+			.waitUntilResponsive()
 			.startConsensusOnRun() // in case we're the first to access the cluster
 			.build();
 		test.runBlocking(1, TimeUnit.MINUTES);
