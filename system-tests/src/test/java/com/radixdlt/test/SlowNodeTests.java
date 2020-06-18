dc3610c40035a9b8134811405a441f2ceff10e48
shambupujar <shambu@radixdlt.com>
2020-06-18 23:46:40 +0100

    Docker SlowNodeTests

---
+++ system-tests/src/test/java/com/radixdlt/test/SlowNodeTests.java
@@ -0,0 +1,45 @@
+package com.radixdlt.test;
+
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import utils.CmdHelper;
+
+/**
+ * BFT tests against network where all nodes are under synchrony bounds and one or more nodes slow.
+ */
+public class SlowNodeTests {
+	/**
+	 * Gets the test builder for slow node BFT network tests.
+	 *
+	 * @return The test builder
+	 */
+	static RemoteBFTTest.Builder slowNodeTestBuilder() {
+		return RemoteBFTTest.builder()
+			.assertAllProposalsHaveDirectParents()
+			.assertNoRejectedProposals()
+			.assertNoSyncExceptions()
+			.assertNoTimeouts()
+			.assertSafety()
+			.assertLiveness();
+	}
+
+	@Test
+	@Category(Docker.class)
+	public void given_3_correct_bfts_in_latent_docker_network_and_one_slow_node__then_all_instances_should_get_same_commits_and_progress_should_be_made() {
+		try (DockerNetwork network = DockerNetwork.builder().numNodes(3).build()) {
+
+			network.startBlocking();
+			CmdHelper.runCommand("docker run --rm --pid=host --privileged  "
+				+ "-v /var/run/docker.sock:/var/run/docker.sock dockerinpractice/comcast "
+				+ "-cont "+ network.getNodeIds().stream().findFirst().get() +" -default-bw 50 -latency 100 -packet-loss 20%");
+			RemoteBFTTest test = slowNodeTestBuilder()
+				.network(RemoteBFTNetworkBridge.of(network))
+				.waitUntilResponsive()
+				.startConsensusOnRun()
+				.build();
+			test.runBlocking(1, TimeUnit.MINUTES);
+		}
+	}
+}
