97323ea3a6624e4258a8b1a593208229b09a3e15
shambupujar <shambu@radixdlt.com>
2020-06-22 09:08:00 +0100

    Added sleep before changing the network

--- system-tests/src/test/java/com/radixdlt/test/SlowNodeTests.java
+++ system-tests/src/test/java/com/radixdlt/test/SlowNodeTests.java
@@ -31,6 +31,7 @@ public class SlowNodeTests {
 		try (DockerNetwork network = DockerNetwork.builder().numNodes(3).build()) {
 
 			network.startBlocking();
+			Thread.sleep(20000);
 			CmdHelper.runCommand("docker run --rm --pid=host --privileged  "
 				+ "-v /var/run/docker.sock:/var/run/docker.sock dockerinpractice/comcast "
 				+ "-cont "+ network.getNodeIds().stream().findFirst().get() +" -default-bw 50 -latency 100 -packet-loss 20%");
@@ -40,6 +41,8 @@ public class SlowNodeTests {
 				.startConsensusOnRun()
 				.build();
 			test.runBlocking(20, TimeUnit.MINUTES);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 }
