a909e3ea01a1c3594dcdba5893c170718bbd3dfd
Florian Cäsar <florian@radixdlt.com>
2020-06-25 12:17:23 +0200

    Add basic staking mapper tests

---
+++ radixdlt-java/src/test/java/com/radixdlt/client/application/translate/tokens/StakeTokensMapperTest.java
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright 2020 Radix DLT Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package com.radixdlt.client.application.translate.tokens;
+
+import com.radixdlt.client.application.translate.ShardedParticleStateId;
+import com.radixdlt.client.atommodel.tokens.TransferrableTokensParticle;
+import com.radixdlt.identifiers.RRI;
+import com.radixdlt.identifiers.RadixAddress;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StakeTokensMapperTest {
+
+	@Test
+	public void createTransactionWithNoFunds() {
+		RadixAddress address = mock(RadixAddress.class);
+
+		RRI token = mock(RRI.class);
+		when(token.getName()).thenReturn("TEST");
+
+		StakeTokensAction action = mock(StakeTokensAction.class);
+		when(action.getAmount()).thenReturn(new BigDecimal("1.0"));
+		when(action.getFrom()).thenReturn(address);
+		when(action.getRRI()).thenReturn(token);
+
+		StakeTokensMapper transferTranslator = new StakeTokensMapper();
+
+		assertThat(transferTranslator.requiredState(action))
+			.containsExactly(ShardedParticleStateId.of(TransferrableTokensParticle.class, address));
+
+		assertThatThrownBy(() -> transferTranslator.mapToParticleGroups(action, Stream.empty()))
+			.isEqualTo(new InsufficientFundsException(token, BigDecimal.ZERO, new BigDecimal("1.0")));
+	}
+
+}
