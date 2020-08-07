f9aefa477d868ff0a76512180b2e43e144264d3c
shambupujar <shambu@radixdlt.com>
2020-08-07 09:35:35 +0100

    [RPNV1-502] Typo fixed

--- run-slow-node-test.sh
+++ run-slow-node-test.sh
@@ -5,5 +5,5 @@ echo "Test executor Container Name = $container_name"
 docker build -f system-tests/docker/Dockerfile -t radix-system-test .
 docker ps -a
 docker rm -f "${container_name}" || true
-docker create  --pid=host --privileged  -v /var/run/docker.sock:/var/run/docker.sock --network=host --cap-add=NET_ADMIN  -e CONTAINER_NAME -e TEST_DURATION--name=${container_name} radix-system-test ./gradlew clean dockerSystemTests
+docker create  --pid=host --privileged  -v /var/run/docker.sock:/var/run/docker.sock --network=host --cap-add=NET_ADMIN  -e CONTAINER_NAME -e TEST_DURATION --name=${container_name} radix-system-test ./gradlew clean dockerSystemTests
 docker start -a "${container_name}"
