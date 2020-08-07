e2de035874e830a4fe8fe0827264a46b00829a14
shambupujar <shambu@radixdlt.com>
2020-08-07 10:38:45 +0100

    [RPNV1-502] Logging in shell script

--- run-slow-node-test.sh
+++ run-slow-node-test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # This script represents commands that can be used to run tests locally on development laptop and also on jenkins
-container_name="${TEST_EXECUTOR:-test-executor}"
-echo "Test executor Container Name = $container_name"
+set -x
+test_executor="${TEST_EXECUTOR:-test-executor}"
+echo "Test executor Container Name = $test_executor"
+echo "Duration of individual tests = $TEST_DURATION"
 docker build -f system-tests/docker/Dockerfile -t radix-system-test .
 docker ps -a
-docker rm -f "${container_name}" || true
-docker create  --pid=host --privileged  -v /var/run/docker.sock:/var/run/docker.sock --network=host --cap-add=NET_ADMIN  -e CONTAINER_NAME -e TEST_DURATION --name=${container_name} radix-system-test ./gradlew clean dockerSystemTests
-docker start -a "${container_name}"
+docker rm -f "${test_executor}" || true
+docker create  --pid=host --privileged  -v /var/run/docker.sock:/var/run/docker.sock --network=host --cap-add=NET_ADMIN  -e CONTAINER_NAME -e TEST_DURATION --name=${test_executor} radix-system-test ./gradlew clean dockerSystemTests
+docker start -a "${test_executor}"
