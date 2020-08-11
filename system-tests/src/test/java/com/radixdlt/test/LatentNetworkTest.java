c4fd96523c400308712e1c0e19d0feb0b0a1382e
shambupujar <shambu@radixdlt.com>
2020-08-11 08:44:13 +0100

    Add changes for capturing logs from docker containers for docker tests

--- system-tests/src/test/java/com/radixdlt/test/LatentNetworkTest.java
+++ system-tests/src/test/java/com/radixdlt/test/LatentNetworkTest.java
@@ -1,20 +1,31 @@
 package com.radixdlt.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import java.util.concurrent.TimeUnit;
+import org.junit.rules.TestName;
 import utils.CmdHelper;
+import utils.Generic;
 
 /**
  * BFT tests against static, non-byzantine networks with random latency.
  */
 public class LatentNetworkTest {
+	private static final Logger logger = LogManager.getLogger();
+
+	@Rule
+	public TestName name = new TestName();
 
 	@Test
 	@Category(Docker.class)
 	public void given_3_correct_bfts_in_latent_docker_network__then_all_instances_should_get_same_commits_and_progress_should_be_made() {
-		try (DockerNetwork network = DockerNetwork.builder().numNodes(3).build()) {
+		String name = Generic.extractTestName(this.name.getMethodName());
+		logger.info("Test name is " + Generic.extractTestName(this.name.getMethodName()));
+		try (DockerNetwork network = DockerNetwork.builder().numNodes(3).testName(name).build()) {
 			network.startBlocking();
 			RemoteBFTTest test = AssertionChecks.latentTestBuilder()
 				.network(RemoteBFTNetworkBridge.of(network))
@@ -28,7 +39,10 @@ public class LatentNetworkTest {
 	@Test
 	@Category(Docker.class)
 	public void given_4_correct_bfts_in_latent_docker_network__then_all_instances_should_get_same_commits_and_progress_should_be_made() {
-		try (DockerNetwork network = DockerNetwork.builder().numNodes(4).build()) {
+		String name = Generic.extractTestName(this.name.getMethodName());
+		logger.info("Test name is " + name);
+
+		try (DockerNetwork network = DockerNetwork.builder().numNodes(4).testName(name).build()) {
 			network.startBlocking();
 			RemoteBFTTest test = AssertionChecks.latentTestBuilder()
 				.network(RemoteBFTNetworkBridge.of(network))
