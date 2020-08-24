6d45c5799019c7ffd7918873f959718a65471f78
Joshua Primero <josh@radixdlt.com>
2020-08-24 09:33:34 -0400

    Replace TempoException with BerkeleyStoreException

--- radixdlt/src/main/java/com/radixdlt/store/berkeley/BerkeleyStoreException.java
+++ radixdlt/src/main/java/com/radixdlt/store/berkeley/BerkeleyStoreException.java
@@ -6,7 +6,7 @@
  * compliance with the License.  You may obtain a copy of the
  * License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -15,17 +15,17 @@
  * language governing permissions and limitations under the License.
  */
 
-package com.radixdlt.consensus.tempo;
+package com.radixdlt.store.berkeley;
 
 /**
  * A Tempo exception
  */
-public class TempoException extends RuntimeException {
-	public TempoException(String message) {
+public class BerkeleyStoreException extends RuntimeException {
+	public BerkeleyStoreException(String message) {
 		super(message);
 	}
 
-	public TempoException(String message, Throwable cause) {
+	public BerkeleyStoreException(String message, Throwable cause) {
 		super(message, cause);
 	}
 }
