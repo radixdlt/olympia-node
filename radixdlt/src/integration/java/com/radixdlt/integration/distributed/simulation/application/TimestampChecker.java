38584bd545612cddcb0fa5b781f8847df7bc9f08
Joshua Primero <josh@radixdlt.com>
2020-10-26 11:36:35 -0400

    Fix timestamp checker

--- radixdlt/src/integration/java/com/radixdlt/integration/distributed/simulation/application/TimestampChecker.java
+++ radixdlt/src/integration/java/com/radixdlt/integration/distributed/simulation/application/TimestampChecker.java
@@ -24,20 +24,27 @@ import com.radixdlt.utils.Pair;
 import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Observable;
 
-public class TimestampChecker implements TestInvariant {
-	private boolean timestampOkay(long timestamp) {
+/**
+ * Checks that the first time a ledger update occurs on the network that it is close
+ * to the real wall clock time.
+ */
+public final class TimestampChecker implements TestInvariant {
+	private Maybe<TestInvariantError> checkCloseTimestamp(long timestamp) {
 		long now = System.currentTimeMillis();
-		return timestamp <= now && timestamp > now - 15_000;
+		if (timestamp <= now && timestamp > now - 15_000) {
+			return Maybe.empty();
+		} else {
+			return Maybe.just(new TestInvariantError("Expecting timestamp to be close to " + now
+				+ " but was " + timestamp));
+		}
 	}
 
 	@Override
 	public Observable<TestInvariantError> check(RunningNetwork network) {
 		return network.ledgerUpdates()
 			.map(Pair::getSecond)
+			.distinct() // Test on only the first ledger update in the network
 			.filter(l -> !(l.getTail().getEpoch() == 1 && l.getTail().getView().equals(View.of(1))))
-			.flatMapMaybe(update -> timestampOkay(update.getTail().timestamp())
-				? Maybe.empty()
-				: Maybe.just(new TestInvariantError("bad timestamp: " + update.getTail()))
-			);
+			.flatMapMaybe(update -> this.checkCloseTimestamp(update.getTail().timestamp()));
 	}
 }
