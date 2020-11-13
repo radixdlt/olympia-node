3f62c513f2467176ed2a7a6264e558458a21798c
Martin Sandiford <martin@radixdlt.com>
2020-11-13 12:54:04 +0000

    Remove unused field from EpochManagerTest

--- radixdlt/src/test/java/com/radixdlt/consensus/epoch/EpochManagerTest.java
+++ radixdlt/src/test/java/com/radixdlt/consensus/epoch/EpochManagerTest.java
@@ -96,9 +96,6 @@ public class EpochManagerTest {
 	@Inject
 	private Hasher hasher;
 
-	@Inject
-	private BFTValidatorSet validatorSet;
-
 	private ECKeyPair ecKeyPair = ECKeyPair.generateNew();
 
 	private BFTUpdateSender bftUpdateSender = mock(BFTUpdateSender.class);
