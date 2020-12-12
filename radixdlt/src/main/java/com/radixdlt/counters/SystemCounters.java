88d7f86c39c871985d45314162dff0abe8ea0142
Joshua Primero <josh@radixdlt.com>
2020-12-12 17:15:35 -0500

    Add sync target state version counter

--- radixdlt/src/main/java/com/radixdlt/counters/SystemCounters.java
+++ radixdlt/src/main/java/com/radixdlt/counters/SystemCounters.java
@@ -60,6 +60,7 @@ public interface SystemCounters {
 
 		SYNC_INVALID_COMMANDS_RECEIVED("sync.invalid_commands_received"),
 		SYNC_PROCESSED("sync.processed"),
+		SYNC_TARGET_STATE_VERSION("sync.target_state_version"),
 
 		MEMPOOL_COUNT("mempool.count"),
 		MEMPOOL_MAXCOUNT("mempool.maxcount"),
