76046c2c56fb593f5cb2118dc9b0108c0760fd6b
Joshua Primero <josh@radixdlt.com>
2020-12-12 18:25:12 -0500

    Add counter for vertex store persistent saves

--- radixdlt/src/main/java/com/radixdlt/counters/SystemCounters.java
+++ radixdlt/src/main/java/com/radixdlt/counters/SystemCounters.java
@@ -51,6 +51,8 @@ public interface SystemCounters {
 		ELAPSED_BDB_LEDGER("elapsed.bdb.ledger"),
 		ELAPSED_BDB_SAFETY_STATE("elapsed.bdb.safety_state"),
 
+		PERSISTENCE_VERTEX_STORE_SAVES("persistence.vertex_store_saves"),
+
 		EPOCH_MANAGER_QUEUED_CONSENSUS_EVENTS("epoch_manager.queued_consensus_events"),
 
 		HASHED_BYTES("hashed.bytes"),
