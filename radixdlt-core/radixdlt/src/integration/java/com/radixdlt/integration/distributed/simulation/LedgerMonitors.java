2b9c351b6e28d8180f19c3dde032aeb18616de08
Joshua Primero <josh@radixdlt.com>
2021-01-20 13:41:56 -0500

    Add javadoc and licenses

--- radixdlt-core/radixdlt/src/integration/java/com/radixdlt/integration/distributed/simulation/LedgerMonitors.java
+++ radixdlt-core/radixdlt/src/integration/java/com/radixdlt/integration/distributed/simulation/LedgerMonitors.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
 package com.radixdlt.integration.distributed.simulation;
 
 import com.google.inject.AbstractModule;
@@ -7,6 +24,9 @@ import com.radixdlt.integration.distributed.simulation.invariants.consensus.Node
 import com.radixdlt.integration.distributed.simulation.invariants.ledger.ConsensusToLedgerCommittedInvariant;
 import com.radixdlt.integration.distributed.simulation.invariants.ledger.LedgerInOrderInvariant;
 
+/**
+ * Monitors which checks things in the ledger module
+ */
 public final class LedgerMonitors {
     public static Module consensusToLedger() {
         return new AbstractModule() {
