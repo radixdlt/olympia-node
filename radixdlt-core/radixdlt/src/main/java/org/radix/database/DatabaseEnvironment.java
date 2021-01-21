303d9bd006e67a3194a3db31d08b3f527dc88248
Sergiy Yevtushenko <sergiy@radixdlt.com>
2021-01-21 15:11:33 +0100

    Fix compilation issue after merging

--- radixdlt-core/radixdlt/src/main/java/org/radix/database/DatabaseEnvironment.java
+++ radixdlt-core/radixdlt/src/main/java/org/radix/database/DatabaseEnvironment.java
@@ -29,7 +29,7 @@ import com.sleepycat.je.Environment;
 import com.sleepycat.je.EnvironmentConfig;
 import com.sleepycat.je.LockMode;
 import com.sleepycat.je.OperationStatus;
-import com.sleepycat.je.Transaction;
+import com.radixdlt.store.Transaction;
 import org.bouncycastle.util.Arrays;
 
 import java.io.File;
