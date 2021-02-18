6fb852ecd047dc7f6048bf5023fd88136817d53e
Joshua Primero <josh@radixdlt.com>
2021-02-18 12:51:30 -0500

    Add javadocs

--- radixdlt-core/radixdlt/src/integration/java/com/radixdlt/integration/mempool/MempoolPerformanceTest.java
+++ radixdlt-core/radixdlt/src/integration/java/com/radixdlt/integration/mempool/MempoolPerformanceTest.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
 package com.radixdlt.integration.mempool;
 
 import com.google.inject.AbstractModule;
@@ -31,7 +48,11 @@ import org.radix.TokenIssuance;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class MempoolPerformanceTest {
+/**
+ * Tests relative performance between an empty mempool and a full mempool and checks
+ * to see that performance between the two isn't wildly different.
+ */
+public final class MempoolPerformanceTest {
     private static final Logger logger = LogManager.getLogger();
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
