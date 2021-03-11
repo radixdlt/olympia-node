f39fa5eaa5f3ff93b598172cd67dd6b44e4261d9
shambupujar <shambu@radixdlt.com>
2021-03-11 15:57:05 +0530

    command to increase ulimit on container

--- radixdlt-core/docker/scripts/config_radixdlt.sh
+++ radixdlt-core/docker/scripts/config_radixdlt.sh
@@ -5,6 +5,7 @@ set -ex
 # need this to run on Alpine (grsec) kernels without crashing
 find /usr -type f -name java -exec setfattr -n user.pax.flags -v em {} \;
 
+ulimit -n 65536
 env | sort
 
 envsubst <"${RADIXDLT_HOME:?}"/default.config.envsubst >"${RADIXDLT_HOME:?}"/default.config
