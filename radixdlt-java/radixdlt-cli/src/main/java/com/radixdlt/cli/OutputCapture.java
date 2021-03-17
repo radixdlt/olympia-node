699e3695d71c1f8f352ddd4b782ad6ab5351a674
shambupujar <shambu@radixdlt.com>
2021-03-17 20:07:25 +0530

    Changed formatting to space for OutputCapture

--- radixdlt-java/radixdlt-cli/src/main/java/com/radixdlt/cli/OutputCapture.java
+++ radixdlt-java/radixdlt-cli/src/main/java/com/radixdlt/cli/OutputCapture.java
@@ -7,62 +7,62 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class OutputCapture implements AutoCloseable {
-	public enum OutputType implements Consumer<PrintStream>, Supplier<PrintStream> {
-		STDOUT {
-			@Override
-			public PrintStream get() {
-				return System.out;
-			}
+    public enum OutputType implements Consumer<PrintStream>, Supplier<PrintStream> {
+        STDOUT {
+            @Override
+            public PrintStream get() {
+                return System.out;
+            }
 
-			@Override
-			public void accept(PrintStream printStream) {
-				System.setOut(printStream);
-			}
-		},
-		STDERR {
-			@Override
-			public PrintStream get() {
-				return System.err;
-			}
+            @Override
+            public void accept(PrintStream printStream) {
+                System.setOut(printStream);
+            }
+        },
+        STDERR {
+            @Override
+            public PrintStream get() {
+                return System.err;
+            }
 
-			@Override
-			public void accept(PrintStream printStream) {
-				System.setErr(printStream);
-			}
-		};
-	}
+            @Override
+            public void accept(PrintStream printStream) {
+                System.setErr(printStream);
+            }
+        };
+    }
 
-	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
-	private final PrintStream capturedStream = new PrintStream(out);
-	private final OutputType outputType;
-	private PrintStream savedStream;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream capturedStream = new PrintStream(out);
+    private final OutputType outputType;
+    private PrintStream savedStream;
 
-	private OutputCapture(final OutputType outputType) {
-		this.outputType = outputType;
-		this.savedStream = this.outputType.get();
-		this.outputType.accept(capturedStream);
-	}
+    private OutputCapture(final OutputType outputType) {
+        this.outputType = outputType;
+        this.savedStream = this.outputType.get();
+        this.outputType.accept(capturedStream);
+    }
 
-	public static OutputCapture startStdout() {
-		return new OutputCapture(OutputType.STDOUT);
-	}
+    public static OutputCapture startStdout() {
+        return new OutputCapture(OutputType.STDOUT);
+    }
 
-	public static OutputCapture startStderr() {
-		return new OutputCapture(OutputType.STDERR);
-	}
+    public static OutputCapture startStderr() {
+        return new OutputCapture(OutputType.STDERR);
+    }
 
-	public String stop() {
-		close();
-		capturedStream.flush();
-		capturedStream.close();
-		return out.toString(StandardCharsets.UTF_8);
-	}
+    public String stop() {
+        close();
+        capturedStream.flush();
+        capturedStream.close();
+        return out.toString(StandardCharsets.UTF_8);
+    }
 
-	@Override
-	public void close() {
-		if (savedStream != null) {
-			outputType.accept(savedStream);
-			savedStream = null;
-		}
-	}
+    @Override
+    public void close() {
+        if (savedStream != null) {
+            outputType.accept(savedStream);
+            savedStream = null;
+        }
+    }
 }
