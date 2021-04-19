b4beafd7e35c1e3c4040827157c6450825857831
Sergiy Yevtushenko <sergiy@radixdlt.com>
2021-04-19 14:06:58 +0200

    Intermediate commit

--- radixdlt-core/radixdlt/src/main/java/com/radixdlt/client/service/ValidatorInfoService.java
+++ radixdlt-core/radixdlt/src/main/java/com/radixdlt/client/service/ValidatorInfoService.java
@@ -17,18 +17,41 @@
 
 package com.radixdlt.client.service;
 
+import com.google.inject.Inject;
 import com.radixdlt.client.api.ValidatorDetails;
+import com.radixdlt.engine.RadixEngine;
 import com.radixdlt.identifiers.RadixAddress;
+import com.radixdlt.statecomputer.LedgerAndBFTProof;
+import com.radixdlt.statecomputer.RegisteredValidators;
+import com.radixdlt.statecomputer.Stakes;
+import com.radixdlt.utils.UInt256;
 import com.radixdlt.utils.functional.Result;
 import com.radixdlt.utils.functional.Tuple.Tuple2;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class ValidatorInfoService {
+	private final RadixEngine<LedgerAndBFTProof> radixEngine;
+
+	@Inject
+	public ValidatorInfoService(RadixEngine<LedgerAndBFTProof> radixEngine) {
+		this.radixEngine = radixEngine;
+	}
+
 	public Result<Tuple2<Optional<RadixAddress>, List<ValidatorDetails>>> getValidators(
 		int size, Optional<RadixAddress> cursor
 	) {
+		var validators = radixEngine.getComputedState(RegisteredValidators.class);
+		var stakes = radixEngine.getComputedState(Stakes.class);
+
+		var result = validators.map((address, details) -> {
+			return ValidatorDetails.create(address, address, details.getName(), details.getUrl(), UInt256.ZERO, UInt256.ZERO, true);
+		});
+
+		result.sort(Comparator.comparing(Object::toString));
+
 		return Result.fail("Not implemented");
 	}
 }
