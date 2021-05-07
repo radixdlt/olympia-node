47fcc57949769aeeb3a083804d941426b5fe0fb8
shambupujar <shambu@radixdlt.com>
2021-05-07 09:56:34 +0100

    Add ability to use uid from host

--- radixdlt-core/docker/scripts/config_radixdlt.sh
+++ radixdlt-core/docker/scripts/config_radixdlt.sh
@@ -5,6 +5,14 @@ set -ex
 # need this to run on Alpine (grsec) kernels without crashing
 find /usr -type f -name java -exec setfattr -n user.pax.flags -v em {} \;
 
+#Add user using env variable if provided
+USER_ID=${LOCAL_USER_ID:-999}
+USER_NAME=radixdlt
+# check and delete the user that is created in postinstal action of deb package
+getent group $USER_NAME >/dev/null && groupmod -g $USER_ID radixdlt || groupadd -r $USER_NAME
+getent passwd $RADIXDLT_USER >/dev/null && usermod -u $USER_ID radixdlt || useradd -r -d "$RADIXDLT_HOME" -g $RADIXDLT_USER $RADIXDLT_USER
+chown -R radixdlt:radixdlt /home/radixdlt/
+
 env | sort
 
 envsubst <"${RADIXDLT_HOME:?}"/default.config.envsubst >"${RADIXDLT_HOME:?}"/default.config
