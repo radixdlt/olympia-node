b4dec83a9338a0c7e932e1032c21ae44d66116e0
Theo Christodoulou <thechristodoulou@gmail.com>
2021-05-11 23:00:45 +0200

    WIP

--- radixdlt-regression/src/test/java/com/radix/acceptance/AcceptanceTest.java
+++ radixdlt-regression/src/test/java/com/radix/acceptance/AcceptanceTest.java
@@ -2,6 +2,7 @@ package com.radix.acceptance;
 
 import com.radix.test.account.Account;
 import com.radix.test.network.TestNetwork;
+import com.radixdlt.client.lib.api.AccountAddress;
 import com.radixdlt.utils.functional.Result;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,12 +15,16 @@ public abstract class AcceptanceTest {
     private Result<Account> account;
 
     public AcceptanceTest() {
-        testNetwork = TestNetwork.initializeFromSystemProperties();
+        testNetwork = TestNetwork.initializeFromEnv();
         account = testNetwork.generateNewAccount();
     }
 
     public Result<Account> getTestAccount() {
         return account;
+    }
+
+    public void faucet(AccountAddress to, int amount) {
+        testNetwork.faucet(to, amount);
     }
 
     public TestNetwork getNetwork() {
