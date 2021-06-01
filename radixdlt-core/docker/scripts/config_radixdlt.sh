dbc1b73dccefbccf3b32430681889db381910386
shambupujar <shambu@radixdlt.com>
2021-06-01 09:39:52 +0100

    Add newline when merging test.config with default.config

--- radixdlt-core/docker/scripts/config_radixdlt.sh
+++ radixdlt-core/docker/scripts/config_radixdlt.sh
@@ -9,13 +9,14 @@ find /usr -type f -name java -exec setfattr -n user.pax.flags -v em {} \;
 USER_ID=${LOCAL_USER_ID:-999}
 USER_NAME=radixdlt
 # check and delete the user that is created in postinstal action of deb package
-getent group $USER_NAME >/dev/null && groupmod -g $USER_ID radixdlt || groupadd -r $USER_NAME
+getent group $USER_NAME >/dev/null && groupmod -g $USER_ID radixdlt || groupadd -r $USER_NAME -g $USER_ID
 getent passwd $USER_NAME >/dev/null && usermod -u $USER_ID radixdlt || useradd -r -d "$RADIXDLT_HOME" -g $USER_NAME $USER_NAME
 chown -R radixdlt:radixdlt /home/radixdlt/
 
 #check for test network configs
 TEST_CONFIGS="${RADIXDLT_HOME:?}"/test.config
 if test -f "$TEST_CONFIGS"; then
+    echo >> default.config.envsubst
     cat $TEST_CONFIGS >> default.config.envsubst
 fi
 
