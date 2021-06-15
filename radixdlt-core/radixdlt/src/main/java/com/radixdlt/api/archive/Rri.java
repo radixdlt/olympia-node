radixdlt/radixdlt-parent

Refactor existing JSON-RPC handlers

radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/archive/Rri.java
@@ -1,0 +1,79 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+
+package com.radixdlt.api.archive;
+
+import org.bitcoinj.core.Bech32;
+
+import com.radixdlt.atomos.ConstraintScryptEnv;
+import com.radixdlt.identifiers.REAddr;
+import com.radixdlt.utils.Bits;
+import com.radixdlt.utils.Pair;
+import com.radixdlt.utils.functional.Result;
+
+import static com.radixdlt.identifiers.CommonErrors.UNABLE_TO_DECODE;
+
+/**
+ * A Radix resource identifier which encodes addresses with a resource behind them in
+ * Bech-32 encoding.
+ *
+ * The human-readable part is the alphanumeric argument provided when creating the
+ * resource followed by "_rb" for betanet and "_rr" for mainnet.
+ *
+ * The data part is a conversion of the 1-34 byte Radix Engine address
+ * {@link com.radixdlt.identifiers.REAddr} to Base32 similar to specification described
+ * in BIP_0173 for converting witness programs.
+ */
+public final class Rri {
+	private static final String RRI_HRP_SUFFIX = "_rb";
+
+	private Rri() {
+		throw new IllegalStateException("Cannot instantiate.");
+	}
+
+	private static byte[] toBech32Data(byte[] bytes) {
+		return Bits.convertBits(bytes, 0, bytes.length, 8, 5, true);
+	}
+
+	private static byte[] fromBech32Data(byte[] bytes) {
+		return Bits.convertBits(bytes, 0, bytes.length, 5, 8, false);
+	}
+
+	public static Pair<String, REAddr> parse(String rri) {
+		var data = Bech32.decode(rri);
+		if (!data.hrp.endsWith(RRI_HRP_SUFFIX)) {
+			throw new IllegalArgumentException("Address hrp suffix must be " + RRI_HRP_SUFFIX + "(" + rri + ")");
+		}
+		var symbol = data.hrp.substring(0, data.hrp.length() - RRI_HRP_SUFFIX.length());
+		if (!ConstraintScryptEnv.NAME_PATTERN.matcher(symbol).matches()) {
+			throw new IllegalArgumentException("Invalid symbol in address (" + rri + ")");
+		}
+		var addrBytes = fromBech32Data(data.data);
+		return Pair.of(symbol, REAddr.of(addrBytes));
+	}
+
+	public static Result<Pair<String, REAddr>> parseFunctional(String rri) {
+		return Result.wrap(UNABLE_TO_DECODE, () -> parse(rri));
+	}
+
+	public static String of(String symbol, REAddr addr) {
+		var addrBytes = addr.getBytes();
+		var bech32Data = toBech32Data(addrBytes);
+		return Bech32.encode(symbol + RRI_HRP_SUFFIX, bech32Data);
+	}
+}

radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/archive/store/ClientApiStore.java
@@ -1,0 +1,90 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+package com.radixdlt.api.archive.store;
+
+import com.radixdlt.api.archive.api.TxHistoryEntry;
+import com.radixdlt.api.archive.store.berkeley.BalanceEntry;
+import com.radixdlt.api.archive.store.berkeley.ScheduledQueueFlush;
+import com.radixdlt.environment.EventProcessor;
+import com.radixdlt.identifiers.AID;
+import com.radixdlt.identifiers.REAddr;
+import com.radixdlt.utils.UInt384;
+import com.radixdlt.utils.functional.Result;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * High level JSON RPC client API store.
+ */
+public interface ClientApiStore {
+
+	/**
+	 * Retrieve list of immediately spendable token balances or stakes.
+	 *
+	 * @param addr client address
+	 * @param retrieveStakes {@code true} - retrieve stakes, {@code false} - retrieve spendable balances
+	 *
+	 * @return list of token balances
+	 */
+	Result<List<BalanceEntry>> getTokenBalances(REAddr addr, boolean retrieveStakes);
+
+	Result<REAddr> parseRri(String rri);
+
+	/**
+	 * Get current supply of the specified token.
+	 *
+	 * @param rri token for which supply is requested
+	 *
+	 * @return eventually consistent token supply
+	 */
+	Result<UInt384> getTokenSupply(String rri);
+
+	/**
+	 * Retrieve token definition. Note that for mutable supply tokens supply is returned zero.
+	 * If actual token supply value is necessary then {@link #getTokenSupply(String)} should be used.
+	 *
+	 * @param rri token for which definition is requested
+	 *
+	 * @return token definition.
+	 */
+	Result<TokenDefinitionRecord> getTokenDefinition(REAddr rri);
+
+	/**
+	 * Retrieve transaction history for provided address.
+	 *
+	 * @param address client address
+	 * @param size number of elements to return
+	 * @param cursor optional cursor from previous request
+	 *
+	 * @return list of transaction history entries.
+	 */
+	Result<List<TxHistoryEntry>> getTransactionHistory(REAddr address, int size, Optional<Instant> cursor);
+
+	/**
+	 * Retrieve single transaction history entry.
+	 *
+	 * @param txId transaction id
+	 *
+	 * @return transaction history entry.
+	 */
+	Result<TxHistoryEntry> getTransaction(AID txId);
+
+	EventProcessor<ScheduledQueueFlush> queueFlushProcessor();
+}

radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/chaos/chaos/mempoolfiller/MempoolFiller.java
@@ -1,0 +1,177 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+package com.radixdlt.api.chaos.chaos.mempoolfiller;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.radixdlt.atom.TxActionListBuilder;
+import com.radixdlt.atom.TxBuilderException;
+import com.radixdlt.atom.Txn;
+import com.radixdlt.consensus.HashSigner;
+import com.radixdlt.consensus.bft.BFTNode;
+import com.radixdlt.consensus.bft.Self;
+import com.radixdlt.counters.SystemCounters;
+import com.radixdlt.crypto.ECPublicKey;
+import com.radixdlt.engine.RadixEngine;
+import com.radixdlt.environment.EventDispatcher;
+import com.radixdlt.environment.EventProcessor;
+import com.radixdlt.environment.RemoteEventDispatcher;
+import com.radixdlt.environment.ScheduledEventDispatcher;
+import com.radixdlt.identifiers.REAddr;
+import com.radixdlt.mempool.MempoolAdd;
+import com.radixdlt.network.addressbook.PeersView;
+import com.radixdlt.statecomputer.LedgerAndBFTProof;
+import com.radixdlt.statecomputer.RadixEngineMempool;
+import com.radixdlt.statecomputer.transaction.TokenFeeChecker;
+import com.radixdlt.utils.UInt256;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Periodically fills the mempool with valid transactions
+ */
+public final class MempoolFiller {
+	private static final Logger logger = LogManager.getLogger();
+	private final RadixEngine<LedgerAndBFTProof> radixEngine;
+
+	private final RemoteEventDispatcher<MempoolAdd> remoteMempoolAddEventDispatcher;
+	private final EventDispatcher<MempoolAdd> mempoolAddEventDispatcher;
+
+	private final RadixEngineMempool radixEngineMempool;
+	private final ScheduledEventDispatcher<ScheduledMempoolFill> mempoolFillDispatcher;
+	private final SystemCounters systemCounters;
+	private final PeersView peersView;
+	private final Random random;
+	private final HashSigner hashSigner;
+	private final ECPublicKey self;
+	private final REAddr account;
+
+	private boolean enabled = false;
+	private int numTransactions;
+	private boolean sendToSelf = false;
+
+	@Inject
+	public MempoolFiller(
+		@Self ECPublicKey self,
+		@Self REAddr account,
+		@Named("RadixEngine") HashSigner hashSigner,
+		RadixEngineMempool radixEngineMempool,
+		RadixEngine<LedgerAndBFTProof> radixEngine,
+		EventDispatcher<MempoolAdd> mempoolAddEventDispatcher,
+		RemoteEventDispatcher<MempoolAdd> remoteMempoolAddEventDispatcher,
+		ScheduledEventDispatcher<ScheduledMempoolFill> mempoolFillDispatcher,
+		PeersView peersView,
+		Random random,
+		SystemCounters systemCounters
+	) {
+		this.self = self;
+		this.account = account;
+		this.hashSigner = hashSigner;
+		this.radixEngine = radixEngine;
+		this.radixEngineMempool = radixEngineMempool;
+		this.mempoolAddEventDispatcher = mempoolAddEventDispatcher;
+		this.remoteMempoolAddEventDispatcher = remoteMempoolAddEventDispatcher;
+		this.mempoolFillDispatcher = mempoolFillDispatcher;
+		this.peersView = peersView;
+		this.random = random;
+		this.systemCounters = systemCounters;
+	}
+
+	public EventProcessor<MempoolFillerUpdate> mempoolFillerUpdateEventProcessor() {
+		return u -> {
+			u.numTransactions().ifPresent(numTx -> this.numTransactions = numTx);
+			u.sendToSelf().ifPresent(sendToSelf -> this.sendToSelf = sendToSelf);
+
+			if (u.enabled() == enabled) {
+				u.onError("Already " + (enabled ? "enabled." : "disabled."));
+				return;
+			}
+
+			logger.info("Mempool Filler: Updating " + u.enabled());
+			u.onSuccess();
+
+			if (u.enabled()) {
+				enabled = true;
+				mempoolFillDispatcher.dispatch(ScheduledMempoolFill.create(), 50);
+			} else {
+				enabled = false;
+			}
+		};
+	}
+
+	public EventProcessor<ScheduledMempoolFill> scheduledMempoolFillEventProcessor() {
+		return p -> {
+			if (!enabled) {
+				return;
+			}
+
+			var particleCount = radixEngine.getComputedState(Integer.class);
+			if (particleCount == 0) {
+				logger.info("Mempool Filler empty balance");
+				return;
+			}
+
+			var actions = TxActionListBuilder.create()
+				.splitNative(REAddr.ofNativeToken(), TokenFeeChecker.FIXED_FEE.multiply(UInt256.TWO))
+				.burn(REAddr.ofNativeToken(), account, TokenFeeChecker.FIXED_FEE)
+				.build();
+
+			var shuttingDown = radixEngineMempool.getShuttingDownSubstates();
+			var txns = new ArrayList<Txn>();
+			for (int i = 0; i < numTransactions; i++) {
+				try {
+					var builder = radixEngine.construct(self, actions, shuttingDown);
+					shuttingDown.addAll(builder.toLowLevelBuilder().remoteDownSubstate());
+					var txn = builder.signAndBuild(hashSigner::sign);
+					txns.add(txn);
+				} catch (TxBuilderException e) {
+					break;
+				}
+			}
+
+			if (txns.size() == 1) {
+				logger.info("Mempool Filler mempool: {} Adding txn {} to mempool...",
+					systemCounters.get(SystemCounters.CounterType.MEMPOOL_COUNT),
+					txns.get(0).getId()
+				);
+			} else {
+				logger.info("Mempool Filler mempool: {} Adding {} txns to mempool...",
+					systemCounters.get(SystemCounters.CounterType.MEMPOOL_COUNT),
+					txns.size()
+				);
+			}
+
+			List<BFTNode> peers = peersView.peers();
+			txns.forEach(txn -> {
+				int index = random.nextInt(sendToSelf ? peers.size() + 1 : peers.size());
+				var mempoolAdd = MempoolAdd.create(txn);
+				if (index == peers.size()) {
+					this.mempoolAddEventDispatcher.dispatch(mempoolAdd);
+				} else {
+					this.remoteMempoolAddEventDispatcher.dispatch(peers.get(index), mempoolAdd);
+				}
+			});
+
+			mempoolFillDispatcher.dispatch(ScheduledMempoolFill.create(), 500);
+		};
+	}
+}

radixdlt/olympia-node

radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/archive/Rri.java
@@ -1,0 +1,79 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+
+package com.radixdlt.api.archive;
+
+import com.radixdlt.atomos.CMAtomOS;
+import org.bitcoinj.core.Bech32;
+
+import com.radixdlt.identifiers.REAddr;
+import com.radixdlt.utils.Bits;
+import com.radixdlt.utils.Pair;
+import com.radixdlt.utils.functional.Result;
+
+import static com.radixdlt.identifiers.CommonErrors.UNABLE_TO_DECODE;
+
+/**
+ * A Radix resource identifier which encodes addresses with a resource behind them in
+ * Bech-32 encoding.
+ *
+ * The human-readable part is the alphanumeric argument provided when creating the
+ * resource followed by "_rb" for betanet and "_rr" for mainnet.
+ *
+ * The data part is a conversion of the 1-34 byte Radix Engine address
+ * {@link com.radixdlt.identifiers.REAddr} to Base32 similar to specification described
+ * in BIP_0173 for converting witness programs.
+ */
+public final class Rri {
+	private static final String RRI_HRP_SUFFIX = "_rb";
+
+	private Rri() {
+		throw new IllegalStateException("Cannot instantiate.");
+	}
+
+	private static byte[] toBech32Data(byte[] bytes) {
+		return Bits.convertBits(bytes, 0, bytes.length, 8, 5, true);
+	}
+
+	private static byte[] fromBech32Data(byte[] bytes) {
+		return Bits.convertBits(bytes, 0, bytes.length, 5, 8, false);
+	}
+
+	public static Pair<String, REAddr> parse(String rri) {
+		var data = Bech32.decode(rri);
+		if (!data.hrp.endsWith(RRI_HRP_SUFFIX)) {
+			throw new IllegalArgumentException("Address hrp suffix must be " + RRI_HRP_SUFFIX + "(" + rri + ")");
+		}
+		var symbol = data.hrp.substring(0, data.hrp.length() - RRI_HRP_SUFFIX.length());
+		if (!CMAtomOS.NAME_PATTERN.matcher(symbol).matches()) {
+			throw new IllegalArgumentException("Invalid symbol in address (" + rri + ")");
+		}
+		var addrBytes = fromBech32Data(data.data);
+		return Pair.of(symbol, REAddr.of(addrBytes));
+	}
+
+	public static Result<Pair<String, REAddr>> parseFunctional(String rri) {
+		return Result.wrap(UNABLE_TO_DECODE, () -> parse(rri));
+	}
+
+	public static String of(String symbol, REAddr addr) {
+		var addrBytes = addr.getBytes();
+		var bech32Data = toBech32Data(addrBytes);
+		return Bech32.encode(symbol + RRI_HRP_SUFFIX, bech32Data);
+	}
+}

radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/archive/store/ClientApiStore.java
@@ -1,0 +1,95 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+package com.radixdlt.api.archive.store;
+
+import com.radixdlt.api.archive.api.TxHistoryEntry;
+import com.radixdlt.api.archive.store.berkeley.BalanceEntry;
+import com.radixdlt.api.archive.store.berkeley.ScheduledQueueFlush;
+import com.radixdlt.environment.EventProcessor;
+import com.radixdlt.identifiers.AID;
+import com.radixdlt.identifiers.REAddr;
+import com.radixdlt.utils.UInt384;
+import com.radixdlt.utils.functional.Result;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * High level JSON RPC client API store.
+ */
+public interface ClientApiStore {
+	enum BalanceType {
+		SPENDABLE, STAKES, UNSTAKES
+	}
+
+	/**
+	 * Retrieve list of immediately spendable token balances or stakes.
+	 *
+	 * @param addr client address
+	 * @param type the type of balance
+	 *
+	 * @return list of token balances
+	 */
+	Result<List<BalanceEntry>> getTokenBalances(REAddr addr, BalanceType type);
+
+	long getEpoch();
+
+	Result<REAddr> parseRri(String rri);
+
+	/**
+	 * Get current supply of the specified token.
+	 *
+	 * @param rri token for which supply is requested
+	 *
+	 * @return eventually consistent token supply
+	 */
+	Result<UInt384> getTokenSupply(String rri);
+
+	/**
+	 * Retrieve token definition. Note that for mutable supply tokens supply is returned zero.
+	 * If actual token supply value is necessary then {@link #getTokenSupply(String)} should be used.
+	 *
+	 * @param rri token for which definition is requested
+	 *
+	 * @return token definition.
+	 */
+	Result<TokenDefinitionRecord> getTokenDefinition(REAddr rri);
+
+	/**
+	 * Retrieve transaction history for provided address.
+	 *
+	 * @param address client address
+	 * @param size number of elements to return
+	 * @param cursor optional cursor from previous request
+	 *
+	 * @return list of transaction history entries.
+	 */
+	Result<List<TxHistoryEntry>> getTransactionHistory(REAddr address, int size, Optional<Instant> cursor);
+
+	/**
+	 * Retrieve single transaction history entry.
+	 *
+	 * @param txId transaction id
+	 *
+	 * @return transaction history entry.
+	 */
+	Result<TxHistoryEntry> getTransaction(AID txId);
+
+	EventProcessor<ScheduledQueueFlush> queueFlushProcessor();
+}

radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/chaos/chaos/mempoolfiller/MempoolFiller.java
@@ -1,0 +1,168 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+package com.radixdlt.api.chaos.chaos.mempoolfiller;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.radixdlt.atom.TxnConstructionRequest;
+import com.radixdlt.atom.TxBuilderException;
+import com.radixdlt.atom.Txn;
+import com.radixdlt.atom.actions.PayFee;
+import com.radixdlt.consensus.HashSigner;
+import com.radixdlt.consensus.bft.BFTNode;
+import com.radixdlt.consensus.bft.Self;
+import com.radixdlt.counters.SystemCounters;
+import com.radixdlt.engine.RadixEngine;
+import com.radixdlt.environment.EventDispatcher;
+import com.radixdlt.environment.EventProcessor;
+import com.radixdlt.environment.RemoteEventDispatcher;
+import com.radixdlt.environment.ScheduledEventDispatcher;
+import com.radixdlt.identifiers.REAddr;
+import com.radixdlt.mempool.MempoolAdd;
+import com.radixdlt.network.addressbook.PeersView;
+import com.radixdlt.statecomputer.LedgerAndBFTProof;
+import com.radixdlt.statecomputer.RadixEngineMempool;
+import com.radixdlt.statecomputer.transaction.TokenFeeChecker;
+import com.radixdlt.utils.UInt256;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Periodically fills the mempool with valid transactions
+ */
+public final class MempoolFiller {
+	private static final Logger logger = LogManager.getLogger();
+	private final RadixEngine<LedgerAndBFTProof> radixEngine;
+
+	private final RemoteEventDispatcher<MempoolAdd> remoteMempoolAddEventDispatcher;
+	private final EventDispatcher<MempoolAdd> mempoolAddEventDispatcher;
+
+	private final RadixEngineMempool radixEngineMempool;
+	private final ScheduledEventDispatcher<ScheduledMempoolFill> mempoolFillDispatcher;
+	private final SystemCounters systemCounters;
+	private final PeersView peersView;
+	private final Random random;
+	private final HashSigner hashSigner;
+	private final REAddr account;
+
+	private boolean enabled = false;
+	private int numTransactions;
+	private boolean sendToSelf = false;
+
+	@Inject
+	public MempoolFiller(
+		@Self REAddr account,
+		@Named("RadixEngine") HashSigner hashSigner,
+		RadixEngineMempool radixEngineMempool,
+		RadixEngine<LedgerAndBFTProof> radixEngine,
+		EventDispatcher<MempoolAdd> mempoolAddEventDispatcher,
+		RemoteEventDispatcher<MempoolAdd> remoteMempoolAddEventDispatcher,
+		ScheduledEventDispatcher<ScheduledMempoolFill> mempoolFillDispatcher,
+		PeersView peersView,
+		Random random,
+		SystemCounters systemCounters
+	) {
+		this.account = account;
+		this.hashSigner = hashSigner;
+		this.radixEngine = radixEngine;
+		this.radixEngineMempool = radixEngineMempool;
+		this.mempoolAddEventDispatcher = mempoolAddEventDispatcher;
+		this.remoteMempoolAddEventDispatcher = remoteMempoolAddEventDispatcher;
+		this.mempoolFillDispatcher = mempoolFillDispatcher;
+		this.peersView = peersView;
+		this.random = random;
+		this.systemCounters = systemCounters;
+	}
+
+	public EventProcessor<MempoolFillerUpdate> mempoolFillerUpdateEventProcessor() {
+		return u -> {
+			u.numTransactions().ifPresent(numTx -> this.numTransactions = numTx);
+			u.sendToSelf().ifPresent(sendToSelf -> this.sendToSelf = sendToSelf);
+
+			if (u.enabled() == enabled) {
+				u.onError("Already " + (enabled ? "enabled." : "disabled."));
+				return;
+			}
+
+			logger.info("Mempool Filler: Updating " + u.enabled());
+			u.onSuccess();
+
+			if (u.enabled()) {
+				enabled = true;
+				mempoolFillDispatcher.dispatch(ScheduledMempoolFill.create(), 50);
+			} else {
+				enabled = false;
+			}
+		};
+	}
+
+	public EventProcessor<ScheduledMempoolFill> scheduledMempoolFillEventProcessor() {
+		return p -> {
+			if (!enabled) {
+				return;
+			}
+
+			var shuttingDown = radixEngineMempool.getShuttingDownSubstates();
+			var txnConstructionRequest = TxnConstructionRequest.create()
+				.action(new PayFee(account, TokenFeeChecker.FIXED_FEE))
+				.splitNative(REAddr.ofNativeToken(), account, TokenFeeChecker.FIXED_FEE.multiply(UInt256.TWO))
+				.avoidSubstates(shuttingDown);
+
+			var txns = new ArrayList<Txn>();
+			for (int i = 0; i < numTransactions; i++) {
+				try {
+					var builder = radixEngine.construct(txnConstructionRequest);
+					shuttingDown.addAll(builder.toLowLevelBuilder().remoteDownSubstate());
+					var txn = builder.signAndBuild(hashSigner::sign);
+					txns.add(txn);
+				} catch (TxBuilderException e) {
+					break;
+				}
+			}
+
+			if (txns.size() == 1) {
+				logger.info("Mempool Filler mempool: {} Adding txn {} to mempool...",
+					systemCounters.get(SystemCounters.CounterType.MEMPOOL_COUNT),
+					txns.get(0).getId()
+				);
+			} else {
+				logger.info("Mempool Filler mempool: {} Adding {} txns to mempool...",
+					systemCounters.get(SystemCounters.CounterType.MEMPOOL_COUNT),
+					txns.size()
+				);
+			}
+
+			List<BFTNode> peers = peersView.peers();
+			txns.forEach(txn -> {
+				int index = random.nextInt(sendToSelf ? peers.size() + 1 : peers.size());
+				var mempoolAdd = MempoolAdd.create(txn);
+				if (index == peers.size()) {
+					this.mempoolAddEventDispatcher.dispatch(mempoolAdd);
+				} else {
+					this.remoteMempoolAddEventDispatcher.dispatch(peers.get(index), mempoolAdd);
+				}
+			});
+
+			mempoolFillDispatcher.dispatch(ScheduledMempoolFill.create(), 500);
+		};
+	}
+}