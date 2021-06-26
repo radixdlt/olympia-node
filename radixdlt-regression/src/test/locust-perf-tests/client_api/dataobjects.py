a78867474307b940cbc8261a29fd917e570f6204
shambupujar <shambu@radixdlt.com>
2021-06-26 14:21:24 +0100

    build Stake tokens request

--- radixdlt-regression/src/test/locust-perf-tests/client_api/dataobjects.py
+++ radixdlt-regression/src/test/locust-perf-tests/client_api/dataobjects.py
@@ -9,3 +9,16 @@ class RequestData():
                 {"address": "brx1qspm44wuhymwwse4jg8y4y8nkj657e3g4659hw53cgg59mkp74w85hqyapytf",
                  "size": 50}
         }
+
+    @staticmethod
+    def build_transaction():
+        return {
+            "method": "construction.build_transaction",
+            "params": {
+                "actions": [
+
+                ],
+                "feePayer": ""
+            },
+            "id": 0
+        }
--- radixdlt-regression/src/test/locust-perf-tests/client_api/endpoints.py
+++ radixdlt-regression/src/test/locust-perf-tests/client_api/endpoints.py
@@ -1,2 +1,6 @@
 def get_archive_endpoint():
     return "/archive"
+
+
+def get_construction_endpoint():
+    return "/construction"
