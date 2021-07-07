f308c5037cd835bcb3893d8c7e1d9219099b1a62
Sergiy Yevtushenko <sergiy@radixdlt.com>
2021-07-07 13:06:16 +0200

    Minor cleanups

--- radixdlt-java/radixdlt-java/src/main/java/com/radixdlt/client/lib/api/async/RadixApi.java
+++ radixdlt-java/radixdlt-java/src/main/java/com/radixdlt/client/lib/api/async/RadixApi.java
@@ -54,6 +54,7 @@ import com.radixdlt.client.lib.dto.TxDTO;
 import com.radixdlt.client.lib.dto.UnstakePositions;
 import com.radixdlt.client.lib.dto.ValidatorDTO;
 import com.radixdlt.client.lib.dto.ValidatorsResponse;
+import com.radixdlt.crypto.ECKeyPair;
 import com.radixdlt.identifiers.AID;
 
 import java.time.Duration;
