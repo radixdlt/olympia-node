390c2526c8c45b39e1052f5691c96741d9348c59
Joshua Primero <josh@radixdlt.com>
2021-07-14 10:22:47 -0500

    Update stokenet genesis

--- radixdlt-core/radixdlt/src/main/java/com/radixdlt/statecomputer/forks/MainnetForkConfigsModule.java
+++ radixdlt-core/radixdlt/src/main/java/com/radixdlt/statecomputer/forks/MainnetForkConfigsModule.java
@@ -30,7 +30,7 @@ import java.util.OptionalInt;
  */
 public final class MainnetForkConfigsModule extends AbstractModule {
 	@ProvidesIntoSet
-	ForkConfig olympiaFirstEpoch() {
+	ForkConfig stokenet() {
 		return new ForkConfig(
 			0L,
 			"olympia-first-epoch",
@@ -41,17 +41,18 @@ public final class MainnetForkConfigsModule extends AbstractModule {
 					Amount.ofTokens(1000) // 1000XRD per resource
 				),
 				OptionalInt.of(50), // 50 Txns per round
-				1_500_000, // Two weeks worth of rounds for first epoch
+				10_000,
 				150, // Two weeks worth of epochs
 				Amount.ofTokens(100), // Minimum stake
 				150, // Two weeks worth of epochs
-				Amount.ofTokens(0),   // No rewards for epoch 1 where it will only be radix foundation nodes
+				Amount.ofTokens(10),   // Rewards per proposal
 				9800, // 98.00% threshold for completed proposals to get any rewards,
 				100 // 100 max validators
 			)
 		);
 	}
 
+	/*
 	@ProvidesIntoSet
 	ForkConfig olympia() {
 		return new ForkConfig(
@@ -74,4 +75,5 @@ public final class MainnetForkConfigsModule extends AbstractModule {
 			)
 		);
 	}
+	 */
 }
