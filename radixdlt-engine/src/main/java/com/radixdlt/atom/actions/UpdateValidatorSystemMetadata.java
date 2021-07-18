94c740cff7f2016c0be83c788cc234dfcff99b01
Joshua Primero <josh@radixdlt.com>
2021-07-18 16:37:10 -0500

    Add unit tests

---
+++ radixdlt-engine/src/main/java/com/radixdlt/atom/actions/UpdateValidatorSystemMetadata.java
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+
+package com.radixdlt.atom.actions;
+
+import com.radixdlt.atom.TxValidatorAction;
+import com.radixdlt.crypto.ECPublicKey;
+
+import java.util.Objects;
+
+public class UpdateValidatorSystemMetadata implements TxValidatorAction {
+	private final ECPublicKey validatorKey;
+	private final byte[] bytes;
+
+	public UpdateValidatorSystemMetadata(
+		ECPublicKey validatorKey,
+		byte[] bytes
+	) {
+		this.validatorKey = Objects.requireNonNull(validatorKey);
+		this.bytes = bytes;
+	}
+
+	@Override
+	public ECPublicKey validatorKey() {
+		return validatorKey;
+	}
+
+	public byte[] bytes() {
+		return bytes;
+	}
+}
---
+++ radixdlt-engine/src/test/java/com/radixdlt/application/validators/UpdateValidatorSystemMetadataTest.java
@@ -0,0 +1,90 @@
+/*
+ * (C) Copyright 2021 Radix DLT Ltd
+ *
+ * Radix DLT Ltd licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+
+package com.radixdlt.application.validators;
+
+import com.google.common.base.Strings;
+import com.radixdlt.application.system.construction.CreateSystemConstructorV2;
+import com.radixdlt.application.system.scrypt.EpochUpdateConstraintScrypt;
+import com.radixdlt.application.system.scrypt.RoundUpdateConstraintScrypt;
+import com.radixdlt.application.system.scrypt.SystemConstraintScrypt;
+import com.radixdlt.application.validators.construction.UpdateValidatorSystemMetadataConstructor;
+import com.radixdlt.application.validators.scrypt.ValidatorConstraintScryptV2;
+import com.radixdlt.atom.REConstructor;
+import com.radixdlt.atom.actions.CreateSystem;
+import com.radixdlt.atom.actions.UpdateValidatorSystemMetadata;
+import com.radixdlt.atomos.CMAtomOS;
+import com.radixdlt.constraintmachine.ConstraintMachine;
+import com.radixdlt.constraintmachine.PermissionLevel;
+import com.radixdlt.crypto.ECKeyPair;
+import com.radixdlt.engine.RadixEngine;
+import com.radixdlt.engine.parser.REParser;
+import com.radixdlt.store.EngineStore;
+import com.radixdlt.store.InMemoryEngineStore;
+import com.radixdlt.utils.Bytes;
+import com.radixdlt.utils.UInt256;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Set;
+
+public class UpdateValidatorSystemMetadataTest {
+	private RadixEngine<Void> engine;
+	private EngineStore<Void> store;
+
+	@Before
+	public void setup() throws Exception {
+		var cmAtomOS = new CMAtomOS();
+		cmAtomOS.load(new SystemConstraintScrypt(Set.of()));
+		cmAtomOS.load(new RoundUpdateConstraintScrypt(2));
+		cmAtomOS.load(new EpochUpdateConstraintScrypt(2, UInt256.NINE, 1, 1, 100));
+		cmAtomOS.load(new ValidatorConstraintScryptV2());
+		var cm = new ConstraintMachine(
+			cmAtomOS.getProcedures(),
+			cmAtomOS.buildSubstateDeserialization(),
+			cmAtomOS.buildVirtualSubstateDeserialization()
+		);
+		var parser = new REParser(cmAtomOS.buildSubstateDeserialization());
+		this.store = new InMemoryEngineStore<>();
+		this.engine = new RadixEngine<>(
+			parser,
+			cmAtomOS.buildSubstateSerialization(),
+			REConstructor.newBuilder()
+				.put(UpdateValidatorSystemMetadata.class, new UpdateValidatorSystemMetadataConstructor())
+				.put(CreateSystem.class, new CreateSystemConstructorV2())
+				.build(),
+			cm,
+			store
+		);
+		var txn = this.engine.construct(new CreateSystem(0)).buildWithoutSignature();
+		this.engine.execute(List.of(txn), null, PermissionLevel.SYSTEM);
+	}
+
+	@Test
+	public void update_validator_metadata() throws Exception {
+		// Arrange
+		var key = ECKeyPair.generateNew();
+
+		var bytes = Bytes.fromHexString(Strings.repeat("0c", 32));
+		// Act and Assert
+		var txn = this.engine.construct(new UpdateValidatorSystemMetadata(key.getPublicKey(), bytes))
+			.signAndBuild(key::sign);
+		this.engine.execute(List.of(txn));
+	}
+}
