9b0dd072094c6cbf26b4bf0b2c0f43e861fa0f6b
Russell Harvey <russell@radixdlt.com>
2021-07-26 09:55:20 -0700

    License as Radix Publishing

--- radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/service/ValidatorInfoService.java
+++ radixdlt-core/radixdlt/src/main/java/com/radixdlt/api/service/ValidatorInfoService.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2021 Radix DLT Ltd incorporated in England.
+ *  Copyright 2021 Radix Publishing Ltd incorporated in Jersey (Channel Islands).
  *  *
  *  * Licensed under the Radix License, Version 1.0 (the "License"); you may not use this
  *  * file except in compliance with the License. You may obtain a copy of the License at:
