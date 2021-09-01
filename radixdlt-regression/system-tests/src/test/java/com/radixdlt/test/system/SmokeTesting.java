c034575b5d13e223ba08edb17ac700f90594fbc2
Theo Christodoulou <thechristodoulou@gmail.com>
2021-09-01 14:47:08 +0200

    removed mistakenly checked in code

--- radixdlt-regression/system-tests/src/test/java/com/radixdlt/test/system/SmokeTesting.java
+++ radixdlt-regression/system-tests/src/test/java/com/radixdlt/test/system/SmokeTesting.java
@@ -4,24 +4,13 @@ import org.junit.jupiter.api.Test;
 
 public class SmokeTesting extends SystemTest {
 
-    //@Test
+    @Test
     public void smoke_test_1() {
         runCheck("liveness");
         var firstNode = radixNetwork.getNodes().get(0);
         restartNode(firstNode);
         runCheck("liveness", 60);
         waitForNodeToBeUp(firstNode);
-    }
-
-    @Test
-    public void one() {
-        logger.info("\none!\n");
-
-    }
-
-    @Test
-    public void two() {
-        logger.info("\ntwo!\n");
     }
 
 }
