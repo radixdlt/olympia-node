529000c5fab824cbb10c1defd653bb02003c8372
Theo Christodoulou <thechristodoulou@gmail.com>
2021-10-23 20:41:27 +0200

    checked in sshconfiguration class

---
+++ radixdlt-regression/src/main/java/com/radixdlt/test/network/SshConfiguration.java
@@ -0,0 +1,51 @@
+package com.radixdlt.test.network;
+
+import com.radixdlt.test.utils.TestingUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * properties used to connect to testnet nodes via SSH
+ */
+public class SshConfiguration {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    private final String sshKeyLocation;
+    private final String sshKeyPassphrase;
+    private final String user;
+    private final int port;
+
+    public static SshConfiguration fromEnv() {
+        var sshKeyLocation = TestingUtils.getEnvWithDefault("RADIXDLT_SYSTEM_TESTING_SSH_KEY_LOCATION",
+            System.getenv("HOME") + "/.ssh/id_rsa");
+        var sshKeyPassphrase = TestingUtils.getEnvWithDefault("RADIXDLT_SYSTEM_TESTING_SSH_KEY_PASSPHRASE", "");
+        var sshUser = TestingUtils.getEnvWithDefault("RADIXDLT_SYSTEM_TESTING_SSH_USER", "");
+        var sshPort = TestingUtils.getEnvWithDefault("RADIXDLT_SYSTEM_TESTING_SSH_PORT", -1);
+        return new SshConfiguration(sshKeyLocation, sshKeyPassphrase, sshUser, sshPort);
+    }
+
+    private SshConfiguration(String sshKeyLocation, String sshKeyPassphrase, String user, int port) {
+        this.sshKeyLocation = sshKeyLocation;
+        this.sshKeyPassphrase = sshKeyPassphrase;
+        this.user = user;
+        this.port = port;
+    }
+
+    public String getSshKeyLocation() {
+        return sshKeyLocation;
+    }
+
+    public String getSshKeyPassphrase() {
+        return sshKeyPassphrase;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+}
