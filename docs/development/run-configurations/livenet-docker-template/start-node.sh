a41740e0fb775a2d6905b9fb4b898b11c3618827
David Edey <david+github@radixdlt.com>
2021-11-15 12:40:44 +0000

    Update the livenet-docker-template to add log tailing across restarts

--- docs/development/run-configurations/livenet-docker-template/start-node.sh
+++ docs/development/run-configurations/livenet-docker-template/start-node.sh
@@ -7,4 +7,10 @@ cd "$SCRIPT_DIR"
 
 source ./node-variables.sh
 
-docker-compose up
+# docker-compose up works - but doesn't reattach to containers which are restarted
+# So instead, we use the -d flag to start docker-compose in the background, and manually tail the logs
+
+mkdir -p ./node/logs
+touch ./node/logs/radixdlt-core.log
+docker-compose up -d
+tail -f ./node/logs/radixdlt-core.log
