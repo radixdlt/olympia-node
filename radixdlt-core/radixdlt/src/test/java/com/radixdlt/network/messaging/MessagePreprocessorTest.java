54aaacaf0b1fd5486300afb03ae210e884461b05
Sergiy Yevtushenko <sergiy@radixdlt.com>
2021-11-24 15:32:30 +0100

    Generate messages in test programmatically

--- radixdlt-core/radixdlt/src/test/java/com/radixdlt/network/messaging/MessagePreprocessorTest.java
+++ radixdlt-core/radixdlt/src/test/java/com/radixdlt/network/messaging/MessagePreprocessorTest.java
@@ -1,13 +1,23 @@
 package com.radixdlt.network.messaging;
 
-import com.radixdlt.DefaultSerialization;
-import com.radixdlt.counters.SystemCountersImpl;
-import com.radixdlt.crypto.ECKeyPair;
-import com.radixdlt.network.p2p.NodeId;
-import com.radixdlt.network.p2p.PeerControl;
 import org.junit.Test;
+import org.radix.network.messaging.Message;
 import org.radix.time.Time;
 
+import com.radixdlt.DefaultSerialization;
+import com.radixdlt.consensus.LedgerProof;
+import com.radixdlt.counters.SystemCountersImpl;
+import com.radixdlt.crypto.ECKeyPair;
+import com.radixdlt.ledger.DtoLedgerProof;
+import com.radixdlt.middleware2.network.LedgerStatusUpdateMessage;
+import com.radixdlt.middleware2.network.SyncRequestMessage;
+import com.radixdlt.network.p2p.NodeId;
+import com.radixdlt.network.p2p.PeerControl;
+import com.radixdlt.serialization.DsonOutput;
+import com.radixdlt.serialization.Serialization;
+import com.radixdlt.utils.Compress;
+
+import java.security.PrivilegedAction;
 import java.time.Duration;
 
 import static org.junit.Assert.assertFalse;
@@ -16,37 +26,54 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import static java.security.AccessController.doPrivileged;
+
 public class MessagePreprocessorTest {
-    // serialized version of SyncRequestMessage(null),
-    // can't be created with code, as SyncRequestMessage does not accept null anymore
-    private static final byte[] SYNC_REQUEST_MESSAGE = {
-            -65, 98, 115, 122, 120, 25, 109, 101, 115, 115, 97, 103, 101, 46, 115, 121,
-            110, 99, 46, 115, 121, 110, 99, 95, 114, 101, 113, 117, 101, 115, 116, -1
-    };
+	private final SystemCountersImpl counters = new SystemCountersImpl();
+	private final MessageCentralConfiguration config = mock(MessageCentralConfiguration.class);
+	private final Serialization serialization = DefaultSerialization.getInstance();
+	private final PeerControl peerControl = mock(PeerControl.class);
+	private final MessagePreprocessor messagePreprocessor = new MessagePreprocessor(counters, config, System::currentTimeMillis, serialization, () -> peerControl);
 
+	@Test
+	public void invalid_sync_request_message_is_not_accepted_and_peer_is_banned() throws Exception {
+		var source = NodeId.fromPublicKey(ECKeyPair.generateNew().getPublicKey());
+		var inboundMessage = generateMessage(source, new SyncRequestMessage(mock(DtoLedgerProof.class)), "currentHeader");
+		var result = messagePreprocessor.process(inboundMessage);
 
+		assertFalse(result.isSuccess());
 
-    @Test
-    public void invalid_message_is_not_accepted_and_peer_is_banned() {
-        var counters = new SystemCountersImpl();
-        var config = mock(MessageCentralConfiguration.class);
-        var serialization = DefaultSerialization.getInstance();
-        var peerControl = mock(PeerControl.class);
-        var source = NodeId.fromPublicKey(ECKeyPair.generateNew().getPublicKey());
-        var inboundMessage = new InboundMessage(Time.currentTimestamp(), source, SYNC_REQUEST_MESSAGE);
+		verify(peerControl).banPeer(eq(source), eq(Duration.ofMinutes(5)), anyString());
+	}
 
-        var messagePreprocessor = new MessagePreprocessor(
-                counters,
-                config,
-                System::currentTimeMillis,
-                serialization,
-                () -> peerControl
-        );
+	@Test
+	public void invalid_ledger_status_update_message_is_not_accepted_and_peer_is_banned() throws Exception {
+		var source = NodeId.fromPublicKey(ECKeyPair.generateNew().getPublicKey());
+		var inboundMessage = generateMessage(source, new LedgerStatusUpdateMessage(mock(LedgerProof.class)), "header");
+		var result = messagePreprocessor.process(inboundMessage);
 
-        var result = messagePreprocessor.process(inboundMessage);
+		assertFalse(result.isSuccess());
 
-        assertFalse(result.isSuccess());
+		verify(peerControl).banPeer(eq(source), eq(Duration.ofMinutes(5)), anyString());
+	}
 
-        verify(peerControl).banPeer(eq(source), eq(Duration.ofMinutes(5)), anyString());
-    }
+	private static void setField(Object instance, String fieldName, Object toSet) throws Exception {
+		var field = instance.getClass().getDeclaredField(fieldName);
+
+		doPrivileged((PrivilegedAction<Void>) () -> {
+			field.setAccessible(true);
+			return null;
+		});
+
+		field.set(instance, toSet);
+	}
+
+	private InboundMessage generateMessage(NodeId source, Message message, String field) throws Exception {
+		setField(message, field, null);
+
+		return new InboundMessage(
+			Time.currentTimestamp(),
+			source,
+			Compress.compress(serialization.toDson(message, DsonOutput.Output.WIRE)));
+	}
 }
