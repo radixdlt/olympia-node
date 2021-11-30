bb86e2df64c3b42861cdf11e59a0e9ef8d070156
Diego Marin Santos <diegomrsantos@gmail.com>
2021-11-30 18:02:30 +0100

    Remove cache from PMTStorage and introduce guava cache

--- radixdlt-core/radixdlt/src/main/java/com/radixdlt/store/tree/PMTStorage.java
+++ radixdlt-core/radixdlt/src/main/java/com/radixdlt/store/tree/PMTStorage.java
@@ -1,9 +1,6 @@
 package com.radixdlt.store.tree;
 
-import java.util.List;
-
 public interface PMTStorage {
-	void save(PMTNode node);
-	void save(List<PMTNode> node);
-	PMTNode read(byte[] hash);
+	void save(byte[] serialisedNodeHash, byte[] serialisedNode);
+	byte[] read(byte[] serialisedNodeHash);
 }
