86125782936acd992d534246e2f4197fc911345b
Diego Marin Santos <diegomrsantos@gmail.com>
2021-12-10 17:31:59 +0100

    Make serialization/deserialization injectable

--- radixdlt-core/radixdlt/src/main/java/com/radixdlt/store/tree/PMTLeaf.java
+++ radixdlt-core/radixdlt/src/main/java/com/radixdlt/store/tree/PMTLeaf.java
@@ -1,7 +1,5 @@
 package com.radixdlt.store.tree;
 
-import com.radixdlt.store.tree.serialization.rlp.RLP;
-
 public final class PMTLeaf extends PMTNode {
 
 	public static final int EVEN_PREFIX = 2;
@@ -11,16 +9,5 @@ public final class PMTLeaf extends PMTNode {
 		this.nodeType = NodeType.LEAF;
 		this.keyNibbles = keyNibbles;
 		this.value = newValue;
-	}
-
-	public byte[] serialize() {
-		// INFO: leaf can have empty key. It's because value may not fit branches' hash pointer field
-
-		var nibblesWithPrefix = TreeUtils.applyPrefix(this.getKey().getRaw(), ODD_PREFIX, EVEN_PREFIX);
-		byte[] bytesWithPrefix = TreeUtils.fromNibblesToBytes(nibblesWithPrefix);
-		return RLP.encodeList(
-				RLP.encodeElement(bytesWithPrefix),
-				RLP.encodeElement(value)
-		);
 	}
 }
