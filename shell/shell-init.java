deb38d98b75d869bb175f970e5a5dcc456398270
David Edey <david@rdx.works>
2022-04-25 13:42:38 +0100

    Fix radix shell

---
+++ shell/shell-init.java
@@ -0,0 +1,22 @@
+import static com.radixdlt.shell.RadixShell.*;
+import com.radixdlt.*;
+import com.radixdlt.modules.*;
+import com.radixdlt.network.p2p.*;
+import com.radixdlt.network.p2p.transport.*;
+import com.radixdlt.environment.Runners;
+import com.radixdlt.networks.Network;
+import com.radixdlt.store.DatabaseLocation;
+import com.radixdlt.middleware2.network.*;
+import com.radixdlt.sync.*;
+import com.radixdlt.sync.messages.remote.*;
+import com.radixdlt.environment.rx.*;
+import com.radixdlt.environment.*;
+import com.radixdlt.consensus.bft.*;
+import com.radixdlt.networks.*;
+import com.radixdlt.consensus.sync.*;
+import java.net.URI;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.google.common.hash.HashCode;
+
+System.out.println("\nWelcome to RadixShell!");
